fd4d97737f16e3819bf262c92d639da38546d16d
Francesco <firefelix@gmail.com>
2015-08-18 17:18:16 +0200

    exporting every package directly from src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import * as CookieBanner from './cookie-banner';
+import { CookieBanner, cookie } from './cookie-banner';
 import * as DatePicker from './datepicker';
+import Dropdown from './dropdown';
+import Flex from './flex';
 import InputLink from './input-link';
+import LoadingSpinner from './loading-spinner';
+import Popover from './popover';
 import TextareaAutosize from './textarea-autosize';
 
 export default {
   CookieBanner,
-  DatePicker,
+  cookie,
+  ...DatePicker,
+  Dropdown,
+  ...Flex,
   InputLink,
+  LoadingSpinner,
+  Popover,
   TextareaAutosize
 };
