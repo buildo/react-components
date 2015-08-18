a070972cf5308d5f84a7674a588fffc89a2c1fe1
Francesco <firefelix@gmail.com>
2015-08-18 17:38:04 +0200

    simplified index.js exports

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-import { CookieBanner, cookie } from './cookie-banner';
-import * as DatePicker from './datepicker';
-import Dropdown from './dropdown';
-import Flex from './flex';
-import InputLink from './input-link';
-import LoadingSpinner from './loading-spinner';
-import Popover from './popover';
-import TextareaAutosize from './textarea-autosize';
-
-export default {
-  CookieBanner,
-  cookie,
-  ...DatePicker,
-  Dropdown,
-  ...Flex,
-  InputLink,
-  LoadingSpinner,
-  Popover,
-  TextareaAutosize
-};
+export { CookieBanner, cookie } from './cookie-banner';
+export * from './datepicker';
+export Dropdown from './dropdown';
+export { FlexView, FlexCell } from './flex';
+export InputLink from './input-link';
+export LoadingSpinner from './loading-spinner';
+export Popover from './popover';
+export TextareaAutosize from './textarea-autosize';
