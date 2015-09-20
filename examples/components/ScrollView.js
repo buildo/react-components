f27102d8c9f596a7d345d6db7ca2a6f78f785a22
Francesco <firefelix@gmail.com>
2015-09-20 20:00:20 +0200

    improved example

--- examples/components/ScrollView.js
+++ examples/components/ScrollView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ScrollView } from '../../src';
+import partial from 'lodash/function/partial';
 
 const Example = React.createClass({
 
@@ -10,11 +11,15 @@ const Example = React.createClass({
       <div>
         <ScrollView
           className='hello'
+          easing='easeOutQuad'
           scrollPropagation={false}
-          style={{backgroundColor: 'blue', maxHeight: 120}}>
-            {() => [
-              <div style={{backgroundColor: 'red', height: 300, width: 30}}/>,
-              <div style={{backgroundColor: 'green', height: 300, width: 30}}/>
+          style={{backgroundColor: 'blue', maxHeight: 500, marginTop: 100}}>
+            {(scrollTo) => [
+              <div style={{backgroundColor: 'red', height: 200, width: '100%'}}/>,
+              <div style={{backgroundColor: 'green', height: 200, width: '100%'}}/>,
+              <div style={{backgroundColor: 'yellow', height: 200, width: '100%'}}/>,
+              <div style={{backgroundColor: 'black', height: 200, width: '100%'}}/>,
+              <button onClick={partial(scrollTo, 0, 0, 1500)} />
             ]}
         </ScrollView>
         <div style={{height: 3000}} />
