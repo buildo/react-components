a63483990ac977dd86b5b3f0c889417892969226
Francesco <firefelix@gmail.com>
2015-09-24 11:23:32 +0200

    update example for element children case

--- examples/components/ScrollView.js
+++ examples/components/ScrollView.js
@@ -26,6 +26,19 @@ const Example = React.createClass({
               </div>
             )}
         </ScrollView>
+        <br />
+        <ScrollView
+          className='hello'
+          easing='easeInOutQuad'
+          scrollPropagation={false}
+          style={{backgroundColor: 'blue', maxHeight: 500, marginTop: 100}}>
+            <div>
+              <div style={{backgroundColor: 'black', height: 200, width: '100%'}}/>
+              <div style={{backgroundColor: 'gray', height: 200, width: '100%'}}/>
+              <div style={{backgroundColor: 'brown', height: 200, width: '100%'}}/>
+              <div style={{backgroundColor: 'orange', height: 200, width: '100%'}}/>
+            </div>
+        </ScrollView>
         <div style={{height: 3000}} />
       </div>
     );
