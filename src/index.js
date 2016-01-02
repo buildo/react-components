3446a50fb3ffa4e340ebac8af8166628aa983da9
Francesco <firefelix@gmail.com>
2016-01-02 13:42:00 +0100

    no longer export rc-datepicker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 export { CookieBanner, cookie } from './cookie-banner';
-export * from './datepicker';
 export Dropdown from './dropdown';
 export { FlexView, FlexCell } from './flex';
 export InputLink from './input-link';
