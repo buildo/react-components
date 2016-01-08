c28ac993c3df59b5a3c6bd975a4a61fc0278d3e4
Francesco <firefelix@gmail.com>
2016-01-08 12:42:53 +0100

    move sections definition in its own file

--- examples/examples.js
+++ examples/examples.js
@@ -1,96 +1,19 @@
 import React from 'react/addons';
 import * as brc from '../src';
+import * as rrc from '../src/revenge';
 import lodash from 'lodash';
 import KitchenSink from '../src/kitchen-sink/KitchenSink';
+import sections from './components';
 
-// EXAMPLES
-import DropdownTest from 'raw!./components/Dropdown.example';
-// import MenuTest from 'raw!./components/Menu.example'; // not used!
-import PopoverTest from './components/popover';
-import FlexViewTest from './components/flex';
-import ScrollViewTest from './components/scroll';
-import LoadingSpinnerTest from 'raw!./components/LoadingSpinner.example';
-import MobileDetectorTest from 'raw!./components/MobileDetector.example';
-import LinkStateTest from 'raw!./components/LinkState.example';
-import TextOverflowTest from './components/text-overflow';
-import ToasterTest from './components/toaster';
-import BackgroundDimmerTest from 'raw!./components/BackgroundDimmer.example';
-import ModalManagerTest from 'raw!./components/ModalManager.example';
-
-const sections = [
-  {
-    title: 'Section 1',
-    id: 'section-1',
-    components: [
-      {
-        examples: [DropdownTest],
-        id: 'Dropdown',
-        title: 'Dropdown'
-      },
-      {
-        examples: PopoverTest,
-        id: 'Popover',
-        title: 'Popover'
-      }
-    ]
-  },
-  {
-    title: 'Section 2',
-    id: 'section-2',
-    components: [
-      {
-        examples: FlexViewTest,
-        id: 'FlexView',
-        title: 'FlexView'
-      },
-      {
-        examples: ScrollViewTest,
-        id: 'ScrollView',
-        title: 'ScrollView'
-      },
-      {
-        examples: [LoadingSpinnerTest],
-        id: 'LoadingSpinner',
-        title: 'LoadingSpinner'
-      },
-      {
-        examples: [MobileDetectorTest],
-        id: 'MobileDetector',
-        title: 'MobileDetector'
-      },
-      {
-        examples: [LinkStateTest],
-        id: 'LinkState',
-        title: 'LinkState'
-      },
-      {
-        examples: TextOverflowTest,
-        id: 'TextOverflow',
-        title: 'TextOverflow'
-      },
-      {
-        examples: ToasterTest,
-        id: 'Toaster',
-        title: 'Toaster'
-      },
-      {
-        examples: [BackgroundDimmerTest],
-        id: 'BackgroundDimmer',
-        title: 'BackgroundDimmer'
-      },
-      {
-        examples: [ModalManagerTest],
-        id: 'ModalManager',
-        title: 'ModalManager'
-      }
-    ]
-  }
-];
+import '../src/flex/flexView.scss';
+import '../src/loading-spinner/style.scss';
+import '../src/kitchen-sink/style.scss';
 
 const scope = {
   React,
   ...lodash,
-  ...brc
+  ...brc,
+  ...rrc
 };
 
 const defaultComponent = 'ModalManager';
