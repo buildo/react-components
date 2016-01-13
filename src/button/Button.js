61e5bff3daa79635f0df1a5b4cd0d18b82a6cbf9
Francesco <firefelix@gmail.com>
2016-01-13 16:42:07 +0100

    Button should FlexView instead of sass-flex-mixins

---
+++ src/button/Button.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import cx from 'classnames';
+import pick from 'lodash/object/pick';
+import { pure, skinnable, props, t } from '../utils';
+import ButtonLogic, { buttonState, buttonBaseState } from './ButtonLogic';
+import ButtonRenderer/*, { stringForButtonStates } */ from './ButtonRenderer';
+
+import './button.scss';
+
+// util
+const notBoth = (a, b) => !(a && b);
+
+const ButtonProps = t.subtype(t.struct({
+  baseState: t.maybe(buttonBaseState),
+  // `baseState` is overridden by `buttonState`
+  buttonState: t.maybe(buttonState),
+  onClick: t.Func,
+  stableSuccess: t.maybe(t.Bool),
+  timerMillis: t.maybe(t.Num),
+  label: t.maybe(t.union([t.Str, t.Obj])),//t.maybe(t.union([t.Str, stringForButtonStates])), https://github.com/buildo/labonline/issues/544
+  icon: t.maybe(t.union([t.Str, t.Obj])),//t.maybe(t.union([t.Str, stringForButtonStates])), https://github.com/buildo/labonline/issues/544
+  children: t.maybe(t.ReactNode),
+  style: t.maybe(t.Obj),
+  className: t.maybe(t.Str),
+  primary: t.maybe(t.Bool),
+  size: t.enums.of(['tiny', 'small', 'large'])
+}), ({ label, children }) => notBoth(label, children), 'ButtonProps');
+
+const defaultProps = {
+  primary: false,
+  size: 'small',
+  baseState: 'ready',
+  stableSuccess: false,
+  timerMillis: 2000,
+  icon: '',
+  className: '',
+  style: {}
+};
+
+@pure
+@skinnable()
+@props(ButtonProps)
+export default class Button extends React.Component {
+
+  static defaultProps = defaultProps
+
+  getLocals() {
+
+    const defaultLabels = {
+      success: 'success',
+      error: 'error',
+      processing: 'processing'
+    };
+
+    const defaultIcons = {
+      success: 'validate',
+      error: 'exclamation'
+    };
+
+    const makeProp = x => ( typeof x === 'string' ? { ready: x, 'not-allowed': x } : x );
+
+    const label = {
+      ...defaultLabels,
+      ...makeProp(this.props.label || this.props.children)
+    };
+
+    const icon = {
+      ...defaultIcons,
+      ...makeProp(this.props.icon)
+    };
+
+    const buttonLogicProps = pick(this.props, ['baseState', 'buttonState', 'onClick', 'stableSuccess', 'timerMillis']);
+
+
+    const buttonRendererProps = {
+      style: this.props.style,
+      className: cx({
+        primary: this.props.primary,
+        [this.props.size]: true
+      }, this.props.className),
+      label,
+      icon
+    };
+
+    return {
+      buttonLogicProps,
+      buttonRendererProps
+    };
+  }
+
+  template({ buttonLogicProps, buttonRendererProps }) {
+    return (
+      <ButtonLogic {...buttonLogicProps}>
+        {(props) => <ButtonRenderer {...props} {...buttonRendererProps} />}
+      </ButtonLogic>
+    );
+  }
+
+}
---
+++ src/button/ButtonLogic.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import { pure, skinnable, props, t } from '../utils';
+
+const buttonBaseStates = ['ready', 'success', 'not-allowed'];
+const buttonStates = buttonBaseStates.concat(['processing', 'error']);
+export const buttonBaseState = t.enums.of(buttonBaseStates, 'buttonBaseState');
+export const buttonState = t.enums.of(buttonStates, 'buttonStates');
+
+// const PromiseType = t.irreducible('Promise', x => x instanceof Promise);
+
+/**
+ * A generic button wrapper; it handles processing/success/failure states
+ *
+ * If buttonState is passed in as a prop, all internal states are overridden.
+ *
+ * stableSuccess should not be changed for the lifetime of the component
+ *
+ * Otherwise, the following happens:
+ *
+ *   internalState = null
+ *     |
+ *     ? click
+ *     | --> invoke onClick and grab promise
+ *     V
+ *   internalState = processing
+ *     |                    \
+ *     ? promise success     ------? promise failure
+ *     |                            \
+ *     V                             V
+ *   internalState = success       internalState = error
+ *     |                 \                           \
+ *     |                  --------                    --------------------
+ *     |                          \                                       \
+ *     ? stableSuccess is true     ? stableSuccess is false                |
+ *     |                           |                                       |
+ *     O wait for prop change      * < wait for timerMillis milliseconds > *
+ *                                 |                                      /
+ *                                 |              ------------------------
+ *                                 |             /
+ *                                 V             V
+ *                               internalState = null
+ *
+ */
+@pure
+@skinnable()
+@props({
+  buttonState: t.maybe(buttonState),
+  baseState: t.maybe(buttonBaseState),
+  // onClick: t.func([], PromiseType),
+  onClick: t.Func,
+  stableSuccess: t.Bool,
+  children: t.Func, // TODO check usage of React.CloneElement
+  timerMillis: t.Num
+})
+export default class ButtonLogic extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.timeoutId = null;
+    this.resetInternalStateAfterProcessing = false;
+    this.state = {
+      internalState: null
+    };
+  }
+
+  doResetInternalState() {
+    this.setState({
+      internalState: null
+    });
+  }
+
+  componentWillReceiveProps(props) {
+    if (process.env.NODE_ENV !== 'production') {
+      t.assert(props.stableSuccess === this.props.stableSuccess);
+    }
+
+    if (props.buttonState) {
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+        this.timeoutId = null;
+      }
+    }
+    if (props.baseState !== this.props.baseState) {
+      if (this.state.internalState === 'processing') {
+        this.resetInternalStateAfterProcessing = true;
+      } else {
+        this.doResetInternalState();
+      }
+    }
+  }
+
+  doResetInternalStateAfterTimer() {
+    this.timeoutId = setTimeout(() => {
+      this.timeoutId = null;
+      this.doResetInternalState();
+    }, this.props.timerMillis);
+  }
+
+  attachPromiseHandlers(promise) {
+    promise.then(() => {
+      this.setState({
+        internalState: 'success'
+      }, () => {
+        if (!this.props.stableSuccess) {
+          this.doResetInternalStateAfterTimer();
+        } else if (this.resetInternalStateAfterProcessing) {
+          this.doResetInternalState();
+        }
+      });
+    }).catch(() => {
+      this.setState({
+        internalState: 'error'
+      }, () => this.doResetInternalStateAfterTimer() );
+    });
+  }
+
+  getButtonState = () => (this.props.buttonState || this.state.internalState || this.props.baseState)
+
+  onClick = () => {
+    if (this.getButtonState() === 'ready') {
+      this.resetInternalStateAfterProcessing = false;
+      const promise = this.props.onClick();
+      if (!this.props.buttonState) {
+        this.setState({
+          internalState: 'processing'
+        }, () => this.attachPromiseHandlers(promise) );
+      }
+    }
+  }
+
+  getLocals() {
+    return {
+      buttonState: this.getButtonState(),
+      onClick: this.onClick,
+      child: this.props.children
+    };
+  }
+
+  template({ buttonState, onClick, child }) {
+    return child({ buttonState, onClick });
+  }
+
+}
