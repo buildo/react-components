24d79c202fc7712a574848ac6bc5ceb1e3941ce4
Francesco <firefelix@gmail.com>
2016-01-19 12:26:28 +0100

    remove TooltippedIcon; fix exports

--- examples/components/Icon/index.js
+++ examples/components/Icon/index.js
@@ -1,3 +1,2 @@
 import a from 'raw!./default.example';
-import b from 'raw!./tooltipped.example';
-export default [a, b];
+export default [a];
--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,2 +1,2 @@
-export Icon from './Icon';
-export TooltippedIcon from './TooltippedIcon';
+import Icon from './Icon';
+export default Icon;
