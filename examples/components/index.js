2881a27e61376ba9873d7f588861e95cdc7f7f13
Francesco <firefelix@gmail.com>
2016-02-03 16:10:04 +0100

    fix table examples

--- examples/components/index.js
+++ examples/components/index.js
@@ -31,6 +31,7 @@ import '../../src/loading-spinner/style.scss';
 import '../../src/confirmation-input/confirmationInput.scss';
 import '../../src/badge/badge.scss';
 import '../../src/toggle/toggle.scss';
+import '../../src/table/table.scss';
 import '../../node_modules/react-select/dist/default.css';
 
 export default [
