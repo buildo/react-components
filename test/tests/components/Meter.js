b5ca3d5896cea5c20713cc3770709bb045d93b09
Stefano Saitta <stefano@buildo.io>
2016-02-15 15:22:18 +0100

    test improvements and refactoring

--- test/tests/components/Meter.js
+++ test/tests/components/Meter.js
@@ -5,7 +5,7 @@ const exampleProps = {
   id: '12345',
   className: 'fancy-class-name',
   style: { margin: 10, position: 'relative' },
-  value: 20,
+  value: 25,
   min: 0,
   max: 50,
   ranges: [
@@ -23,100 +23,53 @@ const componentMeter = new Meter({
 });
 
 describe('Meter', function () {
-  it('should exist', () => {
-    expect(componentMeter).toExist();
-  });
-  it('should be an Object', () => {
-    expect(componentMeter).toBeA(Object);
-  });
-  it('should be a Meter Component', () => {
-    expect(componentMeter).toBeA(Meter);
-  });
   describe('locals', () => {
-    it('should compute id', ()  => {
-      const { id: idMeter } = componentMeter.getLocals();
-      expect(idMeter).toExist();
-      expect(idMeter).toBeA('string');
-      expect(idMeter).toBe('12345');
+    it('should pass props', ()  => {
+      const {
+        id,
+        style,
+        value,
+        min,
+        max,
+        ranges,
+        baseLabelColor,
+        baseFillingColor
+      } = componentMeter.getLocals();
+      expect(id).toBe(exampleProps.id);
+      expect(style).toEqual(exampleProps.style);
+      expect(value).toBe(exampleProps.value);
+      expect(min).toBe(exampleProps.min);
+      expect(max).toBe(exampleProps.max);
+      expect(ranges).toEqual(exampleProps.ranges);
+      expect(baseLabelColor).toBe(baseLabelColor);
+      expect(baseFillingColor).toBe(baseFillingColor);
     });
     it('should compute className', ()  => {
       const { className: classNameMeter } = componentMeter.getLocals();
-      expect(classNameMeter).toExist();
-      expect(classNameMeter).toBeA('string');
-      expect(classNameMeter).toBe('meter fancy-class-name');
-    });
-    it('should compute style', ()  => {
-      const { style: styleMeter } = componentMeter.getLocals();
-      expect(styleMeter).toExist();
-      expect(styleMeter).toBeA(Object);
-      expect(styleMeter.margin).toBe(10);
-      expect(styleMeter.position).toBe('relative');
-    });
-    it('should compute value', ()  => {
-      const { value: valueMeter } = componentMeter.getLocals();
-      expect(valueMeter).toBeA('number');
-      expect(valueMeter).toBe(20);
-    });
-    it('should compute min', ()  => {
-      const { min: minMeter } = componentMeter.getLocals();
-      expect(minMeter).toBeA('number');
-      expect(minMeter).toBe(0);
-    });
-    it('should compute max', ()  => {
-      const { max: maxMeter } = componentMeter.getLocals();
-      expect(maxMeter).toBeA('number');
-      expect(maxMeter).toBe(50);
-    });
-    it('should compute ranges', ()  => {
-      const { ranges: rengesMeter } = componentMeter.getLocals();
-      expect(rengesMeter).toExist();
-      expect(rengesMeter).toBeA(Array);
-      expect(rengesMeter.length).toBe(3);
-      expect(rengesMeter[0]).toBeA(Object);
-      expect(rengesMeter[0].startValue).toBeA('number');
-      expect(rengesMeter[0].endValue).toBeA('number');
-      expect(rengesMeter[0].fillingColor).toBeA('string');
-      expect(rengesMeter[0].labelColor).toBeA('string');
-    });
-    it('should compute baseLabelColor', ()  => {
-      const { baseLabelColor: baseLabelColorMeter } = componentMeter.getLocals();
-      expect(baseLabelColorMeter).toExist();
-      expect(baseLabelColorMeter).toBeA('string');
-      expect(baseLabelColorMeter).toBe('#000000');
-    });
-    it('should compute baseFillingColor', ()  => {
-      const { baseFillingColor: baseFillingColorMeter } = componentMeter.getLocals();
-      expect(baseFillingColorMeter).toExist();
-      expect(baseFillingColorMeter).toBeA('string');
-      expect(baseFillingColorMeter).toBe('#ccc');
+      expect(classNameMeter.includes('meter')).toExist();
+      expect(classNameMeter.includes('fancy-class-name')).toExist();
     });
     it('should compute labelFormatter', ()  => {
-      const { labelFormatter: labelFormatterMeter } = componentMeter.getLocals();
-      expect(labelFormatterMeter).toExist();
-      expect(labelFormatterMeter).toBeA('function');
-      expect(labelFormatterMeter()).toBe('test');
+      const { formattedLabel } = componentMeter.getLocals();
+      expect(formattedLabel).toBe('test');
     });
     it('should compute fillingStyle', ()  => {
-      const { fillingStyle: fillingStyleMeter, ranges, value } = componentMeter.getLocals();
+      const { fillingStyle: fillingStyleMeter, ranges } = componentMeter.getLocals();
       expect(fillingStyleMeter).toExist();
       expect(fillingStyleMeter).toBeA(Object);
-      if (value >= ranges[0].startValue && value <= ranges[0].endValue){
-        expect(fillingStyleMeter.backgroundColor).toBe(ranges[0].fillingColor);
-      }
+      expect(fillingStyleMeter.backgroundColor).toBe(ranges[0].fillingColor);
     });
     it('should compute labelStyle', ()  => {
-      const { labelStyle: labelStyleMeter, ranges, value } = componentMeter.getLocals();
+      const { labelStyle: labelStyleMeter, ranges} = componentMeter.getLocals();
       expect(labelStyleMeter).toExist();
       expect(labelStyleMeter).toBeA(Object);
-      if (value >= ranges[0].startValue && value <= ranges[0].endValue){
-        expect(labelStyleMeter.color).toBe(ranges[0].labelColor);
-      }
+      expect(labelStyleMeter.color).toBe(ranges[0].labelColor);
     });
     it('should compute basisSize', ()  => {
-      const { basisSize: basisSizeMeter, ranges, value, min, max } = componentMeter.getLocals();
+      const { basisSize: basisSizeMeter } = componentMeter.getLocals();
       expect(basisSizeMeter).toExist();
       expect(basisSizeMeter).toBeA('string');
-      expect(basisSizeMeter).toBe(`${Math.abs((value - min) * 100 / (max - min))}%`);
+      expect(basisSizeMeter).toBe('50%');
     });
   });
 });
