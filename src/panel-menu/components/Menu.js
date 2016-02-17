9d99ef5c9266f1bb3cb2c471572dcc74ea224448
Stefano Saitta <stefano@buildo.io>
2016-02-17 10:43:33 +0100

    rules added into disable comment

--- src/panel-menu/components/Menu.js
+++ src/panel-menu/components/Menu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import partial from 'lodash/function/partial';
 import { warn } from '../utils/log';
 
-const Menu = React.createClass({ //eslint-disable-line
+const Menu = React.createClass({ //eslint-disable-line react/prefer-es6-class
 
   propTypes: {
     children: React.PropTypes.oneOfType([
