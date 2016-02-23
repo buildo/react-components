5c053e6aa7f0cf037d917d0b5ad2f3649fb318f8
Francesco <firefelix@gmail.com>
2016-02-23 13:20:59 +0100

    add placeholder prop; children used if valid browser

--- src/browser-detector/BrowserDetector.js
+++ src/browser-detector/BrowserDetector.js
@@ -20,9 +20,13 @@ const Browsers = t.irreducible('Browsers', (browsers) => every(browsers, b => in
 @skinnable()
 @props({
   /**
-   * children node rendered when using not-supported browser
+   * children node rendered when using supported browser
    */
-  children: t.maybe(t.Function),
+  children: t.ReactChildren,
+  /**
+   * called when using a non-supported browser. Expected to return valid ReactNode.
+   */
+  placeholder: t.Function,
   /**
    * whitelist of supported browsers. If `undefined` fallbacks to `*``
    */
@@ -36,26 +40,27 @@ export default class BrowserDetector extends React.Component {
 
   detectBrowser = (userAgent) => bowser._detect(userAgent);
 
-  shouldRenderChildren = (supportedBrowsers, detectedBrowser) => (
+  shouldRenderPlaceholder = (supportedBrowsers, detectedBrowser) => (
     supportedBrowsers && !some(supportedBrowsers, b => detectedBrowser[b])
   );
 
   getLocals() {
     const {
-      props: { children, userAgent, supportedBrowsers },
-      shouldRenderChildren, detectBrowser
+      props: { children, userAgent, supportedBrowsers, placeholder },
+      shouldRenderPlaceholder, detectBrowser
     } = this;
     const detectedBrowser = detectBrowser(userAgent || window.navigator.userAgent);
 
     return {
       children,
+      placeholder,
       detectedBrowser,
-      renderChildren: children && shouldRenderChildren(supportedBrowsers, detectedBrowser)
+      renderChildren: !shouldRenderPlaceholder(supportedBrowsers, detectedBrowser)
     };
   }
 
-  template({ children, detectedBrowser, renderChildren }) {
-    return renderChildren ? children(detectedBrowser) : null;
+  template({ children, detectedBrowser, renderChildren, placeholder }) {
+    return renderChildren ? children : placeholder(detectedBrowser);
   }
 
 }
