549a2c8e16e43e3c4e3a5149b1eca3909e23e587
Stefano Saitta <stefano@buildo.io>
2016-02-23 13:01:44 +0100

    handle onChange

--- src/time-picker/TimePicker.js
+++ src/time-picker/TimePicker.js
@@ -10,34 +10,32 @@ const H24 = '24h';
 const H12 = '12h';
 const symbolRegex = /[\s,\.\|\/\\]/;
 const numberRegex = /^\d+$/;
-
+const separator = ':';
 
 const pad = (num) => num <= 9 ? `0${num}` : num;
-
 const timeFormatter24 = (hour, minute) => `${pad(hour)}:${pad(minute)}`;
 const timeFormatter12 = (hour, minute) => {
   if (hour === 0) {
-    return `12:${pad(minute)} am`;
+    return `12${separator}${pad(minute)} am`;
   } else if (hour === 12) {
-    return `12:${pad(minute)} pm`;
+    return `12${separator}${pad(minute)} pm`;
   } else if (hour >= 13) {
-    return `${pad(hour % 12)}:${pad(minute)} pm`;
+    return `${pad(hour % 12)}${separator}${pad(minute)} pm`;
   } else {
-    return `${pad(hour)}:${pad(minute)} am`;
+    return `${pad(hour)}${separator}${pad(minute)} am`;
   }
 };
 
 const insertColon = str => {
   if (numberRegex.test(str) && (str.length >= 3 && str.length <= 4)) {
     const position = str.length - 2;
-    const strWithColon = `${str.substr(0, position)}:${str.substr(position)}`;
+    const strWithColon = `${str.substr(0, position)}${separator}${str.substr(position)}`;
     return strWithColon;
   }
   return str;
 };
 const cleanString = str => str.replace(symbolRegex, ':');
 const cleanFilter = filterStr => insertColon(cleanString(filterStr));
-
 const filterOptions = (options, filterStr) => (
   options.filter( option => (
       option.value.substr(0, filterStr.length) === cleanFilter(filterStr) ||
@@ -51,14 +49,11 @@ const Integer = t.refinement(t.Number, n => n % 1 === 0, 'Integer');
 const Hour = t.refinement(Integer, int => int >= 0 && int <= 23, 'Hour');
 const Hour12 = t.refinement(Hour, int => int >= 1 && int <= 12, 'Hour12');
 const Minute = t.refinement(Integer, int => int >= 0 && int <= 59, 'Minute');
-
 const TimeFormat = t.enums.of([H12, H24], 'TimeFormat');
-
 const Time = t.struct({
   hours: Hour,
   minutes: Minute
 }, 'Time');
-
 const Time12 = t.struct({
   hours: Hour12,
   minutes: Minute
@@ -66,12 +61,12 @@ const Time12 = t.struct({
 
 const Props = t.refinement(t.struct({
   value: Time,
+  onChange: t.Function,
   minTime: t.maybe(Time),
   maxTime: t.maybe(Time),
   interval: t.maybe(Integer),
   placeholder: t.maybe(t.String),
   timeFormat: t.maybe(TimeFormat),
-  onChange: t.maybe(t.Function),
   id: t.maybe(t.String),
   className: t.maybe(t.String),
   style: t.maybe(t.Object)
@@ -84,8 +79,8 @@ const Props = t.refinement(t.struct({
 export default class TimePicker extends React.Component {
 
   static defaultProps = {
-    placeholder: '--:--',
-    timeFormat: '24h',
+    placeholder: `--${separator}--`,
+    timeFormat: H24,
     interval: 30,
     onChange: () => {}
   }
@@ -97,11 +92,18 @@ export default class TimePicker extends React.Component {
     const hours = range(minHour, maxHour);
     const minutes = range(0, 60, interval);
     const options = flatten(hours.map( hour => minutes.map( minute => (
-      { value: `${hour}:${minute}`, label: timeFormatter(hour, minute) }
+      { value: `${hour}${separator}${minute}`, label: timeFormatter(hour, minute) }
     )
     )));
     return options;
   };
+
+  _onChange = (value) => {
+    if (value) {
+      const timeArray = value.split(separator);
+      this.props.onChange({ hours: parseInt(timeArray[0]), minutes: parseInt(timeArray[1]) });
+    }
+  }
 
   getLocals() {
     const {
@@ -114,7 +116,7 @@ export default class TimePicker extends React.Component {
 
     return {
       placeholder,
-      onChange,
+      onChange: this._onChange,
       filterOptions,
       className: cx('time-picker', className),
       options: timeFormat === H24 ? this.createOptionsList({ timeFormatter: timeFormatter24, ...props }) : this.createOptionsList({ timeFormatter: timeFormatter12, ...props })
