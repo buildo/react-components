64922223cbf43aeadbbeb7ac152565caf1555214
Francesco Giordano <francesco.giordano@buildo.io>
2016-02-24 11:45:27 +0100

    add paths to icon

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import range from 'lodash/utility/range';
 import { pure, skinnable, props, t } from '../utils';
 import cx from 'classnames';
 
@@ -8,12 +9,14 @@ import cx from 'classnames';
   icon: t.maybe(t.Str),
   className: t.maybe(t.Str),
   style: t.maybe(t.Obj),
+  paths: t.maybe(t.refinements(t.Number, x => x - Math.floor(x) === 0 && x > 0)),
   onClick: t.maybe(t.Func)
 })
 export default class Icon extends React.Component {
 
   static defaultProps = {
     className: '',
+    paths: 1,
     style: {},
     onClick: () => {}
   };
@@ -24,9 +27,14 @@ export default class Icon extends React.Component {
     };
   }
 
-  template({ icon, className, style, onClick }) {
-    return icon ?
-      <i className={cx('icon', `icon-${icon}`, className)} style={style} onClick={onClick}/>
+  template({ icon, className, style, onClick, paths }) {
+    return icon ? (
+      <i className={cx('icon', `icon-${icon}`, className)} style={style} onClick={onClick}>
+        {paths === 1 ? null : range(paths).map(k => (
+        <span className={`path${k+1}`} />
+        ))}
+      </i>
+    )
     : null;
   }
 
