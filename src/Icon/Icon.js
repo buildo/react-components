681018fec4bce1b5b57ad1704434d429f175e261
Francesco Giordano <francesco.giordano@buildo.io>
2016-02-24 12:13:47 +0100

    fixes

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -3,21 +3,21 @@ import range from 'lodash/utility/range';
 import { pure, skinnable, props, t } from '../utils';
 import cx from 'classnames';
 
+const PositiveInteger = t.refinement(t.Number, x => x % 1 === 0 && x > 0, 'PositiveInteger');
+
 @pure
 @skinnable()
 @props({
   icon: t.maybe(t.Str),
   className: t.maybe(t.Str),
   style: t.maybe(t.Obj),
-  paths: t.maybe(t.refinements(t.Number, x => x - Math.floor(x) === 0 && x > 0)),
+  paths: t.maybe(PositiveInteger),
   onClick: t.maybe(t.Func)
 })
 export default class Icon extends React.Component {
 
   static defaultProps = {
-    className: '',
     paths: 1,
-    style: {},
     onClick: () => {}
   };
 
@@ -30,9 +30,7 @@ export default class Icon extends React.Component {
   template({ icon, className, style, onClick, paths }) {
     return icon ? (
       <i className={cx('icon', `icon-${icon}`, className)} style={style} onClick={onClick}>
-        {paths === 1 ? null : range(paths).map(k => (
-        <span className={`path${k+1}`} />
-        ))}
+        {paths > 1 && range(paths).map(k => <span className={`path${k+1}`} /> )}
       </i>
     )
     : null;
