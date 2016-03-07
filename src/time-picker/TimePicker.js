ad5a895bd6912c80e7291854e530485585d29c7a
Giovanni Gonzaga <giovanni@buildo.io>
2016-03-07 17:27:23 +0100

    cleanup component code, helper functions renaming, change tests accordingly

--- src/time-picker/TimePicker.js
+++ src/time-picker/TimePicker.js
@@ -5,45 +5,43 @@ import Dropdown from '../dropdown/Dropdown';
 import range from 'lodash/utility/range';
 import flatten from 'lodash/array/flatten';
 
-/*
-  Options is defined here cause we need to not change the ref.
-  This happend because of a very wired behaviour of react-select v0.6.12
-*/
 const options = [];
 
 export const H24 = '24h';
 export const H12 = '12h';
-const generateList = {};
-const inputError = {};
+export const inputError = {};
 const symbolRegex = /[\s,\-\+\.\|\/\\]/;
 const numberRegex = /^\d+$/;
 const separator = ':';
 const interval = 30;
 
 const pad = (num) => num <= 9 ? `0${num}` : num;
-const reverseTime12 = hour => hour + 12 !== 24 ? hour + 12 : 0;
+const hoursInH12 = hour => hour + 12 !== 24 ? hour + 12 : 0;
 const lteTime = (minTime, maxTime) => (
   maxTime.hours > minTime.hours || (maxTime.hours === minTime.hours && maxTime.minutes >= minTime.minutes)
 );
 
 const Integer = t.refinement(t.Number, n => n % 1 === 0, 'Integer');
-const Hour = t.refinement(Integer, int => int >= 0 && int <= 23, 'Hour');
-const Hour12 = t.refinement(Integer, int => int >= 1 && int <= 12, 'Hour12');
-const Minute = t.refinement(Integer, int => int >= 0 && int <= 59, 'Minute');
+const Hour = t.refinement(Integer, n => n >= 0 && n <= 23, 'Hour');
+const Hour12 = t.refinement(Integer, n => n >= 1 && n <= 12, 'Hour12');
+const Minute = t.refinement(Integer, n => n >= 0 && n <= 59, 'Minute');
 const TimeFormat = t.enums.of([H12, H24], 'TimeFormat');
 const Time = t.struct({
   hours: Hour,
   minutes: Minute
 }, 'Time');
 
-const isValidHours = (hours, timeFormat) => timeFormat === H24 ? Hour.is(hours) : Hour12.is(hours);
+const isValidHoursInTimeFormat = (hours, timeFormat) => timeFormat === H24 ? Hour.is(hours) : Hour12.is(hours);
 
+// TODO(ste): document props
 const Props = t.refinement(t.struct({
   onChange: t.Function,
+  // TODO(ste): document that value, minTime, maxTime are always considered in H24
   value: t.maybe(Time),
   minTime: t.maybe(Time),
   maxTime: t.maybe(Time),
   placeholder: t.maybe(t.String),
+  // TODO(ste): document that default is H24
   timeFormat: t.maybe(TimeFormat),
   id: t.maybe(t.String),
   className: t.maybe(t.String),
@@ -52,9 +50,8 @@ const Props = t.refinement(t.struct({
   lteTime(minTime, maxTime) && (!value || (lteTime(value, maxTime) && lteTime(minTime, value)))
 ), 'Props');
 
-/** FORMATTING **/
-const timeFormatter24 = ({ hours, minutes }) => `${pad(hours)}:${pad(minutes)}`;
-const timeFormatter12 = ({ hours, minutes }) => {
+const formatTime24 = ({ hours, minutes }) => `${pad(hours)}:${pad(minutes)}`;
+const formatTime12 = ({ hours, minutes }) => {
   if (hours === 0 || (hours > 12 && hours % 12 === 0)) {
     return `12${separator}${pad(minutes)} am`;
   } else if (hours === 12) {
@@ -65,25 +62,24 @@ const timeFormatter12 = ({ hours, minutes }) => {
     return `${pad(hours)}${separator}${pad(minutes)} am`;
   }
 };
-const timeToString = ({ hours, minutes, timeFormat }) => (
-  timeFormat === H12 ? timeFormatter12({ hours, minutes }) : timeFormatter24({ hours, minutes })
+const formatTime = ({ hours, minutes, timeFormat }) => (
+  timeFormat === H12 ? formatTime12({ hours, minutes }) : formatTime24({ hours, minutes })
 );
 
-export const formatter = (time) => ({
-  value: timeFormatter24(time),
-  label: timeToString(time)
+export const toOption = time => ({
+  value: formatTime24(time),
+  label: formatTime(time)
 });
 
-
 const cleanSeparator = str => str.replace(symbolRegex, separator);
-const addZero = str => {
+const maybeAddTrailingZero = str => {
   if (str.length === 4) {
     return `${str}0`;
   } else {
     return str;
   }
 };
-const insertSeparator = str => {
+const maybeInsertSeparator = str => {
   if (numberRegex.test(str)) {
     if (str.length === 3) {
       const position = str.length - 1;
@@ -98,63 +94,61 @@ const insertSeparator = str => {
   return str;
 };
 
-export const parser = (inputStr, timeFormat) => {
+export const parseInTimeFormat = (inputStr, timeFormat) => {
   if (inputStr === '') {
-    return { generateList, originalInput: inputStr };
+    return { originalInput: inputStr };
   }
-  const cleanedInput = addZero(insertSeparator(cleanSeparator(inputStr)));
+  const cleanedInput = maybeAddTrailingZero(maybeInsertSeparator(cleanSeparator(inputStr)));
   const inputArray = cleanedInput.split(separator);
   const [_hours, _minutes] = inputArray;
   const hours = numberRegex.test(_hours) ? parseInt(_hours, 10) : '';
   const minutes = !_minutes || numberRegex.test(_minutes) ? parseInt(_minutes, 10) : '';
+  const validHours = isValidHoursInTimeFormat(hours, timeFormat);
+  const validMinutes = Minute.is(minutes);
 
-  if (isValidHours(hours, timeFormat) && (Minute.is(minutes))) {
+  if (validHours && validMinutes) {
     return { hours, minutes, originalInput: cleanedInput };
-  } else if (hours !== 0 && !isValidHours(hours, timeFormat)) {
-    return { inputError };
-  } else if (Number.isNaN(minutes)) {
-    return { generateList, originalInput: cleanedInput };
+  } else if (validHours) {
+    return { originalInput: cleanedInput };
   } else {
-    return { inputError };
+    return inputError;
   }
 };
 
-export const timesGenerator = ({ hours, minutes, generateList, inputError }, timeFormat) => {
-  if (inputError) {
-    return [];
-  } else if (generateList) {
+export const createTimeList = ({ hours, minutes }, timeFormat) => {
+  if (!isValidHoursInTimeFormat(hours) || !Minute.is(minutes)) {
     const hoursList = range(0, 24);
     const minutesList = range(0, 60, interval);
     return flatten(hoursList.map(hours => minutesList.map(minutes => ({ hours, minutes, timeFormat })
     )));
   } else {
-    return timeFormat !== H24 ? [{ hours, minutes, timeFormat }, { hours: reverseTime12(hours), minutes, timeFormat }] : [{ hours, minutes, timeFormat }];
+    return timeFormat !== H24 ? [
+      { hours, minutes, timeFormat },
+      { hours: hoursInH12(hours), minutes, timeFormat }
+    ] : [
+      { hours, minutes, timeFormat }
+    ];
   }
 };
 
 const startsWith = (time, originalInput) => {
-  const timeStr = timeToString(time);
+  const timeStr = formatTime(time);
   return timeStr.substr(0, originalInput.length) === originalInput;
 };
 
-export const timesFilter = (timeList, { originalInput }, minTime, maxTime) => {
-  return timeList.filter(time => (
-     ((lteTime(minTime, time) && lteTime(time, maxTime)) && startsWith(time, originalInput))
-  ));
+export const filterTime = ({ originalInput, minTime, maxTime }) => time => {
+  return lteTime(minTime, time) && lteTime(time, maxTime) && startsWith(time, originalInput);
 };
 
-/**
-  We use filterOptions because seems that is not possible
-  to change the options from outside of this function,
-  then we use this tricky hack and we workaround this creating a new set
-  of options as we wish and filter it if needed, or just returning a brand new array.
-`_` is a placeholder because the TLC pass me the optionsList, but i don't really care. YOLO
-**/
-export const optionsManager = ({ minTime, maxTime, timeFormat }) => (_, inputStr) => {
-  const time = parser(inputStr, timeFormat);
-  const timeList = timesGenerator(time, timeFormat);
-  const filteredTimeList = timesFilter(timeList, time, minTime, maxTime);
-  return filteredTimeList.map(time => formatter(time));
+// we are not "filtering" options (options array is always empty and discarded by this function)
+// our `filterOptions` actually generates options based on current input string and props
+export const makeFilterOptions = ({ minTime, maxTime, timeFormat }) => (_, inputStr) => {
+  const time = parseInTimeFormat(inputStr, timeFormat);
+  const timeList = time === inputError ? [] : createTimeList(time, timeFormat);
+  const filteredTimeList = timeList.filter(filterTime({
+    originalInput: time.originalInput, minTime, maxTime
+  }));
+  return filteredTimeList.map(toOption);
 };
 
 @skinnable()
@@ -170,7 +164,7 @@ export default class TimePicker extends React.Component {
 
   _onChange = (value) => {
     if (value) {
-      const time = parser(value);
+      const time = parseInTimeFormat(value, H24); // interface with component user is always in H24
       this.props.onChange(time);
     } else {
       this.props.onChange();
@@ -181,17 +175,21 @@ export default class TimePicker extends React.Component {
     const {
       timeFormat,
       className,
-      value,
+      value: userValue,
       ...props
     } = this.props;
+
+    const value = userValue ? toOption({
+      ...userValue, timeFormat: this.props.timeFormat
+    }) : undefined;
 
     return {
       ...props,
       className: cx('time-picker', className),
-      value: value ? formatter(value) : '',
+      value,
       options,
       onChange: this._onChange,
-      filterOptions: optionsManager(this.props)
+      filterOptions: makeFilterOptions(this.props)
     };
   }
 
--- test/tests/components/TimePicker.js
+++ test/tests/components/TimePicker.js
@@ -1,5 +1,8 @@
 import expect from 'expect';
-import TimePicker, { parser, H24, H12, formatter, timesFilter, timesGenerator, optionsManager } from '../../../src/time-picker/TimePicker';
+import { newComponent } from '../helpers';
+import TimePicker, {
+  parseInTimeFormat, H24, H12, toOption, filterTime, createTimeList, makeFilterOptions, inputError
+} from '../../../src/time-picker/TimePicker';
 
 describe('TimePicker', () => {
 
@@ -33,16 +36,37 @@ describe('TimePicker', () => {
       expect(className).toInclude('fancy-class-name');
     });
 
+    it('should pass value properly formatted when H24', () => {
+      const timePicker = newComponent(TimePicker, {
+        value: { hours: 15, minutes: 33 }
+      });
+      const { value } = timePicker.getLocals();
+      expect(value).toEqual({ value: '15:33', label: '15:33' });
+    });
+
+    it('should pass value properly formatted when H12', () => {
+      const timePicker = newComponent(TimePicker, {
+        value: { hours: 15, minutes: 33 },
+        timeFormat: H12
+      });
+      const { value } = timePicker.getLocals();
+      expect(value).toEqual({ value: '15:33', label: '03:33 pm' });
+    });
+
+    it('should pass an undefined value when value prop is undefined', () => {
+      const timePicker = newComponent(TimePicker, {});
+      const { value } = timePicker.getLocals();
+      expect(value).toBe(undefined);
+    });
+
   });
 
-  describe('parser', () => {
+  describe('parseInTimeFormat', () => {
 
     it('should parse input and return a valid 24h time', () => {
       const input = '13:59';
-      const parsedInput = parser(input, H24);
+      const parsedInput = parseInTimeFormat(input, H24);
       expect(parsedInput).toBeA(Object);
-      expect(parsedInput.inputError).toNotExist();
-      expect(parsedInput.generateList).toNotExist();
       expect(parsedInput.originalInput).toBe(input);
       expect(parsedInput.hours).toBe(13);
       expect(parsedInput.minutes).toBe(59);
@@ -50,10 +74,8 @@ describe('TimePicker', () => {
 
     it('should parse input and return a valid 12h time', () => {
       const input = '06:30';
-      const parsedInput = parser(input, H12);
+      const parsedInput = parseInTimeFormat(input, H12);
       expect(parsedInput).toBeA(Object);
-      expect(parsedInput.inputError).toNotExist();
-      expect(parsedInput.generateList).toNotExist();
       expect(parsedInput.originalInput).toBe(input);
       expect(parsedInput.hours).toBe(6);
       expect(parsedInput.minutes).toBe(30);
@@ -61,76 +83,56 @@ describe('TimePicker', () => {
 
     it('should parse input and return an error for invalid 24h time', () => {
       const input = '36:30';
-      const parsedInput = parser(input, H24);
-      expect(parsedInput).toBeA(Object);
-      expect(parsedInput.generateList).toNotExist();
-      expect(parsedInput.originalInput).toNotExist();
-      expect(parsedInput.hours).toNotExist();
-      expect(parsedInput.minutes).toNotExist();
-      expect(parsedInput.inputError).toExist();
+      const parsedInput = parseInTimeFormat(input, H24);
+      expect(parsedInput).toBe(inputError);
     });
 
     it('should parse input and return an error for invalid 12h time', () => {
       const input = '18:30';
-      const parsedInput = parser(input, H12);
-      expect(parsedInput).toBeA(Object);
-      expect(parsedInput.generateList).toNotExist();
-      expect(parsedInput.originalInput).toNotExist();
-      expect(parsedInput.hours).toNotExist();
-      expect(parsedInput.minutes).toNotExist();
-      expect(parsedInput.inputError).toExist();
+      const parsedInput = parseInTimeFormat(input, H12);
+      expect(parsedInput).toBe(inputError);
     });
 
-    it('should parse empty input and return generateList', () => {
+    it('should parse empty input', () => {
       const input = '';
-      const parsedInput = parser(input, H24);
+      const parsedInput = parseInTimeFormat(input, H24);
       expect(parsedInput).toBeA(Object);
-      expect(parsedInput.generateList).toExist();
       expect(parsedInput.originalInput).toBe(input);
       expect(parsedInput.hours).toNotExist();
       expect(parsedInput.minutes).toNotExist();
-      expect(parsedInput.inputError).toNotExist();
     });
 
-    it('should parse input and when minutes are not set will return generateList', () => {
+    it('should parse input when minutes are not set', () => {
       const input = '10:';
-      const parsedInput = parser(input, H24);
+      const parsedInput = parseInTimeFormat(input, H24);
       expect(parsedInput).toBeA(Object);
-      expect(parsedInput.generateList).toExist();
       expect(parsedInput.originalInput).toBe(input);
       expect(parsedInput.hours).toNotExist();
       expect(parsedInput.minutes).toNotExist();
-      expect(parsedInput.inputError).toNotExist();
     });
 
     it('should parse input without separator and return a valid time', () => {
       const input = '1231';
-      const parsedInput = parser(input, H24);
+      const parsedInput = parseInTimeFormat(input, H24);
       expect(parsedInput).toBeA(Object);
       expect(parsedInput.hours).toBe(12);
       expect(parsedInput.minutes).toBe(31);
       expect(parsedInput.originalInput).toBe('12:31');
-      expect(parsedInput.inputError).toNotExist();
-      expect(parsedInput.generateList).toNotExist();
     });
 
     it('should parse input and return error if string is not well formed', () => {
       const input = '1a:aa';
-      const parsedInput = parser(input, H24);
-      expect(parsedInput).toBeA(Object);
-      expect(parsedInput.inputError).toExist();
-      expect(parsedInput.generateList).toNotExist();
-      expect(parsedInput.hours).toNotExist();
-      expect(parsedInput.minutes).toNotExist();
+      const parsedInput = parseInTimeFormat(input, H24);
+      expect(parsedInput).toBe(inputError);
     });
 
   });
 
-  describe('timesGenerator', () => {
+  describe('createTimeList', () => {
 
     it('should return all the available times', () => {
-      const time = { generateList: {} };
-      const timeList24 = timesGenerator(time, H24);
+      const time = { };
+      const timeList24 = createTimeList(time, H24);
       expect(timeList24).toBeA(Array);
       expect(timeList24.length).toBe(48);
       expect(timeList24[0].hours).toBe(0);
@@ -140,14 +142,14 @@ describe('TimePicker', () => {
       expect(timeList24[26].minutes).toBe(0);
       expect(timeList24[27].hours).toBe(13);
       expect(timeList24[27].minutes).toBe(30);
-      const timeList12 = timesGenerator(time, H12);
+      const timeList12 = createTimeList(time, H12);
       expect(timeList24).toNotEqual(timeList12);
       expect(timeList12[0].timeFormat).toBe('12h');
     });
 
     it('should return an array of a single element for 24h', () => {
       const time = { hours: 10, minutes: 31 };
-      const timeList = timesGenerator(time, H24);
+      const timeList = createTimeList(time, H24);
       expect(timeList).toBeA(Array);
       expect(timeList.length).toBe(1);
       expect(timeList[0]).toEqual({ hours: 10, minutes: 31, timeFormat: H24 });
@@ -156,7 +158,7 @@ describe('TimePicker', () => {
 
     it('should return an array of 2 elements for 12h', () => {
       const time = { hours: 10, minutes: 31 };
-      const timeList = timesGenerator(time, H12);
+      const timeList = createTimeList(time, H12);
       expect(timeList).toBeA(Array);
       expect(timeList.length).toBe(2);
       expect(timeList[0]).toEqual({ hours: 10, minutes: 31, timeFormat: H12 });
@@ -164,19 +166,11 @@ describe('TimePicker', () => {
 
     });
 
-    it('should return an empty array on errors', () => {
-      const time = { inputError: {} };
-      const timeList = timesGenerator(time, H24);
-      expect(timeList).toBeA(Array);
-      expect(timeList.length).toBe(0);
-    });
-
   });
 
-  describe('timesFilter', () => {
+  describe('filterTime', () => {
 
     it('should filter based on minTime/maxTime ', () => {
-      const time = { generateList: {}, originalInput: '' };
       const minTime = { hours: 8, minutes: 0 };
       const maxTime = { hours: 9, minutes: 0 };
       const timeList = [
@@ -184,31 +178,33 @@ describe('TimePicker', () => {
         { hours: 8, minutes: 33 },
         { hours: 9, minutes: 44 }
       ];
-      const timeListFilterd = timesFilter(timeList, time, minTime, maxTime);
+      const timeListFilterd = timeList.filter(filterTime({ originalInput: '', minTime, maxTime }));
       expect(timeListFilterd).toBeA(Array);
       expect(timeListFilterd.length).toBe(1);
       expect(timeListFilterd[0]).toEqual({ hours: 8, minutes: 33 });
     });
 
-    it('should filter based on minTime/maxTime and show no result for out of range values', () => {
-      const time = { originalInput: '10:00', hours: 10, minutes: 0 };
-      const minTime = { hours: 8, minutes: 0 };
-      const maxTime = { hours: 9, minutes: 0 };
+    it('should filter based on original input', () => {
       const timeList = [
-        { hours: 10, minutes: 0 }
+        { hours: 7, minutes: 22 },
+        { hours: 8, minutes: 33 },
+        { hours: 9, minutes: 44 }
       ];
-      const timeListFilterd = timesFilter(timeList, time, minTime, maxTime);
+      const timeListFilterd = timeList.filter(filterTime({
+        originalInput: '08', minTime: TimePicker.defaultProps.minTime, maxTime: TimePicker.defaultProps.maxTime
+      }));
       expect(timeListFilterd).toBeA(Array);
-      expect(timeListFilterd.length).toBe(0);
+      expect(timeListFilterd.length).toBe(1);
+      expect(timeListFilterd[0]).toEqual({ hours: 8, minutes: 33 });
     });
 
   });
 
-  describe('formatter', () => {
+  describe('toOption', () => {
 
     it('should format into a valid option for 24H time', () => {
       const time = { hours: 10, minutes: 20 };
-      const formattedTime = formatter(time, H24);
+      const formattedTime = toOption(time, H24);
       expect(formattedTime).toBeA(Object);
       expect(formattedTime.label).toBe('10:20');
     });
@@ -217,35 +213,35 @@ describe('TimePicker', () => {
 
       it('should work 1', () => {
         const time = { hours: 13, minutes: 20, timeFormat: H12 };
-        const formattedTime = formatter(time);
+        const formattedTime = toOption(time);
         expect(formattedTime).toBeA(Object);
         expect(formattedTime.label).toBe('01:20 pm');
       });
 
       it('should work 2', () => {
         const time = { hours: 10, minutes: 20, timeFormat: H12 };
-        const formattedTime = formatter(time);
+        const formattedTime = toOption(time);
         expect(formattedTime).toBeA(Object);
         expect(formattedTime.label).toBe('10:20 am');
       });
 
       it('should work 3', () => {
         const time = { hours: 12, minutes: 34, timeFormat: H12 };
-        const formattedTime = formatter(time);
+        const formattedTime = toOption(time);
         expect(formattedTime).toBeA(Object);
         expect(formattedTime.label).toBe('12:34 pm');
       });
 
       it('should work 4', () => {
         const time = { hours: 0, minutes: 34, timeFormat: H12 };
-        const formattedTime = formatter(time);
+        const formattedTime = toOption(time);
         expect(formattedTime).toBeA(Object);
         expect(formattedTime.label).toBe('12:34 am');
       });
 
       it('should work 5', () => {
         const time = { hours: 24, minutes: 34, timeFormat: H12 };
-        const formattedTime = formatter(time);
+        const formattedTime = toOption(time);
         expect(formattedTime).toBeA(Object);
         expect(formattedTime.label).toBe('12:34 am');
       });
@@ -254,10 +250,10 @@ describe('TimePicker', () => {
 
   });
 
-  describe('optionsManager', () => {
+  describe('makeFilterOptions', () => {
 
     it('should compute options based on min/max for 24H time', () => {
-      const filterOptions = optionsManager({ timeFormat: H24, minTime: { hours: 5, minutes: 10 }, maxTime: { hours: 7, minutes: 20 } });
+      const filterOptions = makeFilterOptions({ timeFormat: H24, minTime: { hours: 5, minutes: 10 }, maxTime: { hours: 7, minutes: 20 } });
       const inputStr = '';
       const results = filterOptions([], inputStr);
       expect(results).toBeA(Array);
@@ -267,10 +263,9 @@ describe('TimePicker', () => {
     });
 
     it('should compute options based on min/max for 12H time', () => {
-      const filterOptions = optionsManager({ timeFormat: H12, minTime: { hours: 5, minutes: 10 }, maxTime: { hours: 7, minutes: 20 } });
+      const filterOptions = makeFilterOptions({ timeFormat: H12, minTime: { hours: 5, minutes: 10 }, maxTime: { hours: 7, minutes: 20 } });
       const inputStr = '';
       const results = filterOptions([], inputStr);
-      expect(optionsManager).toBeA(Function);
       expect(results).toBeA(Array);
       expect(results.length).toBe(4);
       expect(results[0]).toEqual({ label: '05:30 am', value: '05:30' });
