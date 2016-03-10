9c1678d5ac3e84095f5618d52719dc55c1ca3252
Francesco <firefelix@gmail.com>
2016-03-10 11:33:40 +0100

    add docs to props

--- src/focusable-view/FocusableView.js
+++ src/focusable-view/FocusableView.js
@@ -4,11 +4,29 @@ import { props, t, skinnable } from '../utils';
 
 @skinnable()
 @props({
+  /**
+   * FocusableView content
+   */
   children: t.union([t.ReactNode, t.Function]),
+  /**
+   *  Callback function called on "focus" event
+   */
   onFocus: t.maybe(t.Function),
+  /**
+   *  Callback function called on "blur" event
+   */
   onBlur: t.maybe(t.Function),
+  /**
+   *  "tabindex" attribute
+   */
   tabIndex: t.maybe(t.Number),
+  /**
+   *  Wrapper component for `children`
+   */
   component: t.maybe(t.union([t.Function, t.String])),
+  /**
+   *  When `true` the class "focused" is NOT added
+   */
   ignoreFocus: t.maybe(t.Boolean),
   className: t.maybe(t.String),
   style: t.maybe(t.Object)
