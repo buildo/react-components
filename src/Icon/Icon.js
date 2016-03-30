d9cb067471126d6e5d8e6862556da8e26c4aaff7
Francesco Giordano <francesco.giordano@buildo.io>
2016-03-30 12:56:58 +0200

    add color prop to icon

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -9,6 +9,7 @@ const PositiveInteger = t.refinement(t.Number, x => x % 1 === 0 && x > 0, 'Posit
 @skinnable()
 @props({
   icon: t.maybe(t.Str),
+  color: t.maybe(t.String),
   className: t.maybe(t.Str),
   style: t.maybe(t.Obj),
   paths: t.maybe(PositiveInteger),
@@ -27,9 +28,9 @@ export default class Icon extends React.Component {
     };
   }
 
-  template({ icon, className, style, onClick, paths }) {
+  template({ icon, color, className, style = {}, onClick, paths }) {
     return icon ? (
-      <i className={cx('icon', `icon-${icon}`, className)} style={style} onClick={onClick}>
+      <i className={cx('icon', `icon-${icon}`, className)} style={{ ...style, color: color || style.color }} onClick={onClick}>
         {paths > 1 && range(paths).map(k => <span className={`path${k + 1}`} /> )}
       </i>
     )
