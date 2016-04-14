1575547f0727ebf6833cd4dee9e213b4bf829231
Francesco <firefelix@gmail.com>
2016-04-14 12:27:34 +0200

    fix stateClassUtil

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,4 +3,4 @@ import cx from 'classnames';
 export pure from 'revenge/lib/decorators/pure';
 export skinnable from 'revenge/lib/decorators/skinnable';
 export { props, t } from 'tcomb-react';
-export const stateClassUtil = (classes) => cx(classes.reduce((acc, str) => ({ ...acc, [`is-${str}`]: str })));
+export const stateClassUtil = (classes) => cx(classes.reduce((acc, str) => ({ ...acc, [`is-${str}`]: str }), {}));
