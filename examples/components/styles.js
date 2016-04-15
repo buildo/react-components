7542c1acdb77db041520980b88df7b58ab30ee40
Francesco <firefelix@gmail.com>
2016-04-15 15:38:26 +0200

    use require to avoid errors in showroom

--- examples/components/styles.js
+++ examples/components/styles.js
@@ -1,9 +1,10 @@
-import '../../src/more-or-less/moreOrLess.scss';
-import '../../src/flex/flexView.scss';
-import '../../src/loading-spinner/style.scss';
-import '../../src/dropdown/dropdown.scss';
-import '../../src/confirmation-input/confirmationInput.scss';
-import '../../src/badge/badge.scss';
-import '../../src/toggle/toggle.scss';
-import '../../src/table/table.scss';
-import '../../src/Tooltip/tooltip.scss';
+// use require to avoid errors in showroom webpack
+require('../../src/more-or-less/moreOrLess.scss');
+require('../../src/flex/flexView.scss');
+require('../../src/loading-spinner/style.scss');
+require('../../src/dropdown/dropdown.scss');
+require('../../src/confirmation-input/confirmationInput.scss');
+require('../../src/badge/badge.scss');
+require('../../src/toggle/toggle.scss');
+require('../../src/table/table.scss');
+require('../../src/Tooltip/tooltip.scss');
