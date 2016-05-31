6611f1160a4fb6d15634231cf326a4523fdeb2d7
Francesco <firefelix@gmail.com>
2016-05-31 18:12:04 +0200

    implement ResizeSensor

--- src/index.js
+++ src/index.js
@@ -27,3 +27,4 @@ export CollapsableSection from './collapsable-section';
 export BrowserDetector from './browser-detector';
 export TimePicker from './time-picker';
 export FocusableView from './focusable-view';
+export ResizeSensor from './resize-sensor';
