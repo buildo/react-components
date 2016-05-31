982c8159a6a2f2d343810450858ecd25acf57031
Francesco <firefelix@gmail.com>
2016-06-01 00:25:10 +0200

    replace @pure with custom and stricter shouldComponentUpdate

--- src/resize-sensor/ResizeSensor.js
+++ src/resize-sensor/ResizeSensor.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { props, t, pure } from '../utils';
+import { props, t } from '../utils';
 import _ElementQueries from 'css-element-queries/src/ElementQueries';
 import _ResizeSensor from 'css-element-queries/src/ResizeSensor';
 
-@pure
 @props({
   children: t.ReactChildren,
   onResize: t.Function
@@ -31,5 +30,13 @@ export default class ResizeSensor extends React.Component {
 
   render() {
     return this.props.children;
+  }
+
+  shouldComponentUpdate(nextProps) {
+    /*
+      we don't need to update if `onResize` has changed
+      as we're using the constant `this.onResize` callback
+    */
+    return nextProps.children !== this.props.children;
   }
 }
