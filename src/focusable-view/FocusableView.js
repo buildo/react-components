6c212e19031bc06496935932f340168edab659a4
Vincenzo Guerrisi <vincenzo@buildo.io>
2016-08-18 12:06:49 +0200

    update comments for FocusableView

--- src/focusable-view/FocusableView.js
+++ src/focusable-view/FocusableView.js
@@ -2,31 +2,21 @@ import React from 'react';
 import cx from 'classnames';
 import { props, t, skinnable } from '../utils';
 
+/** A panel that can get focus
+ * @param children - FocusableView content. If a function it gets called with the boolean "focused".
+ * @param onFocus - Callback function called on "focus" event
+ * @param onBlur - Callback function called on "blur" event
+ * @param tabIndex - "tabindex" attribute
+ * @param component - Wrapper component for `children`
+ * @param ignoreFocus - When `true` the class "focused" is NOT added
+ */
 @skinnable()
 @props({
-  /**
-   * FocusableView content. If a function it gets called with the boolean "focused".
-   */
   children: t.union([t.ReactChildren, t.Function]),
-  /**
-   *  Callback function called on "focus" event
-   */
   onFocus: t.maybe(t.Function),
-  /**
-   *  Callback function called on "blur" event
-   */
   onBlur: t.maybe(t.Function),
-  /**
-   *  "tabindex" attribute
-   */
   tabIndex: t.maybe(t.Number),
-  /**
-   *  Wrapper component for `children`
-   */
   component: t.maybe(t.union([t.Function, t.String])),
-  /**
-   *  When `true` the class "focused" is NOT added
-   */
   ignoreFocus: t.maybe(t.Boolean),
   className: t.maybe(t.String),
   style: t.maybe(t.Object)
