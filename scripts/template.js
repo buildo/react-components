1b193d088058345affdc29c8bfe650f7c1996b3d
Francesco <firefelix@gmail.com>
2016-08-19 13:52:32 +0200

    use custom formatter for enums

--- scripts/template.js
+++ scripts/template.js
@@ -1,20 +1,28 @@
 import t from 'tcomb';
-const buildPropsTableRows = (props) =>
+const buildPropsTableRows = (props) => (
   Object.keys(props).map((key) => {
     const prop = props[key];
-    return `| ${key} | ${prop.name} | ${t.stringify(prop.defaultValue || '')} | ${prop.description} |`;
-  }).join('\n');
+
+    const name = key;
+    const type = prop.kind === 'enums' ?
+      JSON.stringify(Object.keys(prop.map)) :
+      prop.name;
+    const defaultValue = typeof prop.defaultValue !== 'undefined' ? prop.defaultValue : '';
+    const description = prop.description;
+
+    return `| ${name} | ${type} | ${t.stringify(defaultValue)} | ${description} |`;
+  }).join('\n')
+);
 
 
 export default function template(data) {
-  return (`
-# ${data.name}
+  return (
+`# ${data.name}
 
 ## ${data.description}
 
 |Name|Type|Default|Description|
 |----|----|-------|-----------|
-${buildPropsTableRows(data.props)}
-
-`);
+${buildPropsTableRows(data.props)}`
+);
 }
