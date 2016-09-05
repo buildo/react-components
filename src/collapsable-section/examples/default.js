a2bf440cbb629cc1b132bf50a0167b7682110570
Vincenzo Guerrisi <vincenzo@buildo.io>
2016-09-05 17:01:19 +0200

    CollapsableSection example restyling

--- src/collapsable-section/examples/default.js
+++ src/collapsable-section/examples/default.js
@@ -1,30 +1,63 @@
+const SectionList = ({ items }) => (
+  <FlexView className='section' column>
+    {Object.keys(items).map(k => (
+      <FlexView key={k} className='item' vAlignContent='center'>
+        <FlexView shrink width='100%' className='key'>{k}</FlexView>
+        <FlexView shrink width='100%' className='value'>{items[k]}</FlexView>
+      </FlexView>
+    ))}
+  </FlexView>
+);
+
 class Example extends React.Component {
 
-  constructor() {
-    super();
-    this.state = { isOpen: false };
+  sections = {
+    'General Info': {
+      Name: 'Edgar Kennedy',
+      Email: 'edgar.kennedy@dashboard.com',
+      Country: 'London, United Kingdom',
+      'Favorite movie': 'Citizen Kane - Orson Welles',
+      'Favorite song': 'Smooth Sailing - Queens of the Stone Age'
+    },
+    'Job Info': {
+      Company: 'Buildo',
+      Position: 'Software Engineer'
+    },
+    'Skill Set': {
+      Programming: 'Good',
+      Communications: 'Good',
+      Flexibility: 'Average'
+    }
+  };
+
+  state = {
+    openedSections: []
+  };
+
+  isSectionOpen = (section) => this.state.openedSections.indexOf(section) >= 0;
+
+  onChange = (section) => (isOpen) => {
+    if (isOpen) this.setState({ openedSections: [ ...this.state.openedSections, section ] });
+    else this.setState({ openedSections: this.state.openedSections.filter(el => el !== section) });
   }
 
-  onChange = (isOpen) => this.setState({ isOpen });
-
-  getHeader = () => (
-    <FlexView height={40} vAlignContent='center'>
-      <Icon icon='user' />
-      <FlexView marginLeft={10}>User</FlexView>
-    </FlexView>
-  );
-
   render = () => (
-    <CollapsableSection
-      isOpen={this.state.isOpen}
-      onChange={this.onChange}
-      header={this.getHeader()}
-      icons={{ open: 'angle-up', closed: 'angle-down' }}
-    >
-      <div>ITEM 1</div>
-      <div>ITEM 2</div>
-      <div>ITEM 3</div>
-    </CollapsableSection>
-  );
+    <FlexView column>
 
+      {Object.keys(this.sections).map(section => (
+
+        <CollapsableSection
+          isOpen={this.isSectionOpen(section)}
+          onChange={this.onChange(section)}
+          header={section}
+          key={section}
+          icons={{ open: 'angleRight', closed: 'angleDown' }}
+        >
+          <SectionList items={this.sections[section]} />
+        </CollapsableSection>
+
+      ))}
+
+    </FlexView>
+  )
 }
