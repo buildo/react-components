2efdc06c60607356f54dd6e3390c8cd6bce383f0
Francesco <firefelix@gmail.com>
2016-09-08 14:45:46 +0200

    update functions to dynamically require readmes/components; handle external libraries

--- showroom/components/useLocalComponents.js
+++ showroom/components/useLocalComponents.js
@@ -1,11 +1,20 @@
 import { sortBy, find } from 'lodash';
 
-function dynamicRequire(e) {
-  const [ path ] = e.url.match(/src\/(.+)\/examples\/(.+).js/) || [];
-  return {
-    ...e,
-    code: path ? require(`raw!../../src/${path.replace('src/', '').replace('.js', '')}.js`) : null
-  };
+function dynamicRequire({ repo, example: e }) {
+  switch (repo) {
+    case 'rc-datepicker':
+      return { ...e, code: require(`raw!rc-datepicker/${e.url.replace('.example', '')}.example`) };
+    case 'react-input-children':
+      return { ...e, code: require(`raw!gh-deps/node_modules/react-input-children/${e.url.replace('.example', '')}.example`) };
+    case 'react-autosize-textarea':
+      return { ...e, code: require(`raw!react-autosize-textarea/${e.url.replace('.example', '')}.example`) };
+    case 'react-cookie-banner':
+      return { ...e, code: require(`raw!react-cookie-banner/${e.url.replace('.example', '')}.example`) };
+    case 'react-flexview':
+      return { ...e, code: require(`raw!gh-deps/node_modules/react-flexview/${e.url.replace('.example', '')}.example`) };
+    default:
+      return { ...e, code: require(`raw!../../src/${e.url.replace('.js', '')}.js`) };
+  }
 }
 
 export default json => {
@@ -14,7 +23,7 @@ export default json => {
       const components = sortBy(section.components, 'title')
         .map(c => ({
           ...c,
-          examples: c.examples.map(dynamicRequire)
+          examples: c.examples.map(example => dynamicRequire({ repo: c.repo, example }))
         }))
         .filter(c => !find(c.examples, e => e.code === null));
 
