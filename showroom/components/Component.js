64d482d536f96cedee0d9167e16b9bb3d173048e
Francesco <firefelix@gmail.com>
2016-09-09 15:56:38 +0200

    code clean: avoid setState step

--- showroom/components/Component.js
+++ showroom/components/Component.js
@@ -18,38 +18,6 @@ import KitchenSink from '../../src/kitchen-sink';
 })
 export default class Component extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { loading: true };
-  }
-
-  componentDidMount() {
-    this.loadComponent();
-  }
-
-  loadComponent = (props = this.props) => {
-    const { params: { componentId }, sections, section } = props;
-
-    const componentInfo = find(section.components, { id: componentId });
-
-    const { readme, examples } = componentInfo;
-
-    // README
-    const splittedMarkdown = readme && readme.split('## Props');
-    const header = readme ?
-      <Markdown source={splittedMarkdown[0]} options={{ html: true }} /> :
-      '';
-    const footer = readme ?
-      splittedMarkdown[1] && <Markdown source={`### Props\n${splittedMarkdown[1]}`} options={{ html: true }} /> :
-      '';
-
-    // EXAMPLES
-    const components = section.components.map(c => c.id === componentId ? { ...c, examples } : c);
-    const mappedSections = sections.map(s => s.id === section.id ? { ...s, components } : s);
-
-    this.setState({ sections: mappedSections, header, footer, loading: false });
-  }
-
   getPatchedScope = (scope, componentId) => ({
     ...scope,
     FlexView: componentId === 'react-flexview' ? require('gh-deps/node_modules/react-flexview/src').default : scope.FlexView,
@@ -61,40 +29,35 @@ export default class Component extends React.Component {
     cookie: componentId === 'react-cookie-banner' ? require('react-cookie-banner/src').cookie : scope.cookie
   })
 
+  splitMarkdown = readme => {
+    const splittedMarkdown = readme && readme.split('## Props');
+    return {
+      header: readme ? <Markdown source={splittedMarkdown[0]} options={{ html: true }} /> : '',
+      footer: readme ? splittedMarkdown[1] && <Markdown source={`### Props\n${splittedMarkdown[1]}`} options={{ html: true }} /> : ''
+    };
+  }
+
   render() {
     const {
       getPatchedScope,
-      state: {
-        header, footer, loading,
-        sections: mappedSections
-      },
+      splitMarkdown,
       props: {
         openSections, onToggleSection,
-        onSelectItem, scope,
-        sections: propSections,
+        onSelectItem, scope, section, sections,
         params: { componentId, sectionId }
       }
     } = this;
 
+    const { readme } = find(section.components, { id: componentId });
+    const { header, footer } = splitMarkdown(readme);
+
     const props = {
-      openSections, sectionId, componentId,
-      onToggleSection, onSelectItem, header, footer, loading,
-      sections: mappedSections || propSections,
+      openSections, sectionId, componentId, sections,
+      onToggleSection, onSelectItem, header, footer,
       scope: getPatchedScope(scope, componentId)
     };
 
     return <KitchenSink {...props} />;
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { params: { componentId, sectionId } } = nextProps;
-    if (componentId !== this.props.params.componentId || sectionId !== this.props.params.sectionId) {
-      // resetState
-      this.state = { loading: true };
-      this.forceUpdate();
-
-      this.loadComponent(nextProps);
-    }
   }
 
 }
