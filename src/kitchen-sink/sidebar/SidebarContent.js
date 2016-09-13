ab6def784d2d841cea7d8e036d12a15bcd9268c9
Francesco <firefelix@gmail.com>
2016-09-13 12:27:19 +0300

    if section has only one item dont use CollapsableSection

--- src/kitchen-sink/sidebar/SidebarContent.js
+++ src/kitchen-sink/sidebar/SidebarContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { props, t } from '../../utils';
 import Item from './Item';
+import SingleItemSection from './SingleItemSection';
 import CollapsableSection from '../../collapsable-section/CollapsableSection';
 import View from 'react-flexview';
 
@@ -21,34 +22,46 @@ export default class SidebarContent extends React.Component {
 
   toHome = () => this.props.onSelectItem('home');
 
+  getItems = (sectionId, items) => items.map(({ id, ...item }) =>
+    <Item
+      title={item.title}
+      id={id}
+      onClick={this.props.onSelectItem}
+      sectionId={sectionId}
+      active={this.isActive(id)}
+      key={id}
+    />
+  );
+
+  getSections = (sections) => sections.map(({ id: sectionId, components, contents, title }) => {
+    if ((components || contents).length > 1) {
+      return (
+        <CollapsableSection onChange={this.onToggle(sectionId)} isOpen={this.isOpen(sectionId)} header={title} icons={{ open: 'angleUpsvg', closed: 'angleDown' }} key={sectionId}>
+          <div className='items'>
+            {this.getItems(sectionId, components || contents)}
+          </div>
+        </CollapsableSection>
+      );
+    } else {
+      const { id } = (components || contents)[0];
+      return (
+        <SingleItemSection
+          {...{ title, id, sectionId }}
+          onClick={this.props.onSelectItem}
+          active={this.isActive(id)}
+        />
+      );
+    }
+  });
+
   render() {
-    const { sections, onSelectItem } = this.props;
-    const getItems = (sectionId, items) => items.map(({ id, ...item }) =>
-      <Item
-        title={item.title}
-        id={id}
-        onClick={onSelectItem}
-        sectionId={sectionId}
-        active={this.isActive(id)}
-        key={id}
-      />
-    );
-
-    const getSections = (sections) => sections.map(({ id, components, contents, title }) => (
-      <CollapsableSection onChange={this.onToggle(id)} isOpen={this.isOpen(id)} header={title} icons={{ open: 'angleUpsvg', closed: 'angleDown' }} key={id}>
-        <div className='items'>
-          {getItems(id, components || contents)}
-        </div>
-      </CollapsableSection>
-    ));
-
     return (
       <div className='sidebar-content'>
         <View className='logo' vAlignContent='center' hAlignContent='center' column onClick={this.toHome}>
           buildo
           <View className='sub'>React components</View>
         </View>
-        {getSections(sections)}
+        {this.getSections(this.props.sections)}
       </div>
     );
   }
