e59ce43560ed5ff7577cf03e7ad9b648e2ffaf72
Giovanni Gonzaga <giovanni@buildo.io>
2017-03-01 19:41:09 +0100

    add basic test for modalWithContext

---
+++ test/components/Modal.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { modalWithContext } from '../../src/modal';
+
+describe('Modal', () => {
+
+  it('should correctly pass context down if used via modalWithContext', () => {
+    // `.isRequired` makes the test fail if context is not provided
+    const FooType = React.PropTypes.string.isRequired;
+    const ComponentAccessingContext = (_, ctx) => <div>{ctx.foo}</div>;
+    ComponentAccessingContext.contextTypes = { foo: FooType };
+    const ModalPassingContext = modalWithContext({ foo: FooType });
+    mount(
+      <ModalPassingContext
+        transitionEnterTimeout={0}
+        transitionLeaveTimeout={0}
+      >
+        <ComponentAccessingContext />
+      </ModalPassingContext>,
+      { context: { foo: 'bar' } }
+    );
+  });
+
+});
