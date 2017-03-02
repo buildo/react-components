bb5060d37578e5bc4b284a1d8bf2fa768d48c4f5
Vincenzo Guerrisi <vincenzo@buildo.io>
2017-03-02 10:15:17 +0100

    removing dangling comma from comments and generate readmes

--- src/modal/Modal.js
+++ src/modal/Modal.js
@@ -37,7 +37,7 @@ export const Props = {
  * @param onDismiss - called when modal is dismissed
  * @param transitionEnterTimeout - transition enter timeout
  * @param transitionLeaveTimeout - transition leave timeout
- * @param childContextTypes: context types to pass to the modal React tree,
+ * @param childContextTypes: context types to pass to the modal React tree
  * @param getChildContext: should return context values to pass to the modal React tree
  */
 @skinnable()
