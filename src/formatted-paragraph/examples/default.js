d01a052f08c90bea97b8d2fe06cd688d6def614d
Francesco Cioria <firefelix@gmail.com>
2017-04-14 15:23:58 +0200

    add FormattedParagraph to the showroom

---
+++ src/formatted-paragraph/examples/default.js
@@ -0,0 +1,9 @@
+class Example extends React.Component {
+
+  content = 'First line.\n\nI left an empty line.\n\n\n\nI left three empty lines.\nThis line comes after a single "\\n"';
+
+  render = () => (
+    <FormattedParagraph content={this.content} paragraphSpacing={20} />
+  );
+
+}
