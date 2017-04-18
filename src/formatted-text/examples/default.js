2f28cd4039f97acdd761d3696bc0dd88188ce816
Francesco Cioria <firefelix@gmail.com>
2017-04-18 19:56:13 +0200

    improve example

--- src/formatted-text/examples/default.js
+++ src/formatted-text/examples/default.js
@@ -1,6 +1,6 @@
 class Example extends React.Component {
 
-  content = 'First line.\n\nI left an empty line.\n\n\n\nI left three empty lines.\nThis line comes after a single "\\n"\nhttps://ciaone.com';
+  content = 'First line.\n\nI left an empty line.\n\n\n\nI left three empty lines.\nThis line comes after a single "\\n"\n\nhttps://google.com';
 
   render = () => (
     <FormattedText content={this.content} />
