13bfa5034aee5a1f80049d653b01b359ad3188ed
Andrea Ascari <andrea.ascari@buildo.io>
2017-06-23 17:17:51 +0200

    Definition files for Badge component.

---
+++ src/badge/Badge.d.ts
@@ -0,0 +1,10 @@
+import { PureComponent } from 'react';
+
+export type BadgeProps = {
+  label: any,
+  active?: boolean,
+  className?: string,
+  style?: object
+}
+
+export default class Badge extends PureComponent<BadgeProps, void> {}
---
+++ src/badge/index.d.ts
@@ -0,0 +1,3 @@
+import Badge, { BadgeProps } from './Badge';
+export default Badge;
+export { BadgeProps };
