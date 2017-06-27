62f95bcdce4acce610cfcb9bee69f8c07504745d
Andrea Ascari <andrea.ascari@buildo.io>
2017-06-27 19:56:54 +0200

    Definition files for Button component(s).

---
+++ src/button/index.d.ts
@@ -0,0 +1,6 @@
+import Button, { ButtonProps } from './Button';
+import StatefulButton, { StatefulButtonProps } from './StatefulButton';
+export {
+  Button, ButtonProps,
+  StatefulButton, StatefulButtonProps
+};
