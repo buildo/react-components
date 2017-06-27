e21262afe9b0e5283913445ef86df985437492b3
Andrea Ascari <andrea.ascari@buildo.io>
2017-06-27 19:56:54 +0200

    Definition files for Button component(s).

---
+++ src/button/Button.d.ts
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+export namespace ButtonProps {
+
+  type ButtonState = 'ready' | 'not-allowed' | 'processing' | 'error' | 'success';
+  type ButtonType = 'default' | 'primary' | 'positive' | 'negative' | 'flat';
+  type ButtonSize = 'tiny' | 'small' | 'medium';
+
+}
+
+export type ButtonProps = {
+    onClick: (e: React.SyntheticEvent<HTMLDivElement>) => void,
+    buttonState?: ButtonProps.ButtonState,
+    label?:string | object,
+    icon?:string | object,
+    children?: string,
+    type?: ButtonProps.ButtonType,
+    primary?: boolean,
+    flat?: boolean,
+    size?: ButtonProps.ButtonSize,
+    fluid?: boolean,
+    circular?: boolean,
+    textOverflow?: React.ComponentClass<object>, // todo(typo) when translate text overflow, update here its props types
+    style?: object,
+    className?: string
+  }
+
+export default class Button extends React.PureComponent<ButtonProps, void> {}
+
