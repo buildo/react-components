06a201cc742df2709197172718f1bfb37e0a3f68
Andrea Ascari <andrea.ascari@buildo.io>
2017-06-28 11:02:10 +0200

    Definition files for TextOverflow component.

--- src/text-overflow/TextOverflow.d.ts
+++ src/text-overflow/TextOverflow.d.ts
@@ -1,13 +1,18 @@
 import { CSSProperties, PureComponent } from 'react';
+import { Type } from 'tcomb';
 
 export type TextOverflowProps = {
   children?: (self: JSX.Element, isOpen: boolean) => any,
   label: string,
+  popover?: object, // TODO(typo): Omit?
   lazy?: boolean,
-  delayWhenLazy?: number,
   id?: string,
   className?: string,
   style?: CSSProperties
 }
 
 export default class TextOverflow extends PureComponent<TextOverflowProps> {}
+
+export const Props: {
+  [key: string]: Type<any>
+}
