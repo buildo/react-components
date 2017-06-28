3949da9cd2fa134d03401801ba9eb0c18fd47ffa
Andrea Ascari <andrea.ascari@buildo.io>
2017-06-28 17:33:22 +0200

    Definition files for Popover component.

---
+++ src/popover/Popover.d.ts
@@ -0,0 +1,40 @@
+import { CSSProperties, PureComponent } from 'react'
+
+export namespace PopoverProps {
+
+  type Position = 'top' | 'bottom' | 'left' | 'right'
+  type Anchor = 'start' | 'center' | 'end'
+  type Event = 'click' | 'hover'
+
+  type Popover = {
+    content: any, // TODO: t.ReactChildren
+    attachToBody?: boolean,
+    position?: Position,
+    anchor?: Anchor,
+    event?: Event,
+    onShow?: () => void,
+    onHide?: () => void,
+    onToggle?: () => void,
+    dismissOnScroll?: boolean,
+    dismissOnClickOutside?: boolean,
+    className?: string,
+    style?: CSSProperties,
+    id?: string,
+    maxWidth?: number | string,
+    distance?: number,
+    offsetX?: number,
+    offsetY?: number,
+    isOpen?: boolean,
+    delay?: number | { whenClosed?: number, whenOpen?: number }
+  }
+}
+
+export type PopoverProps = {
+  children: any, // TODO: t.ReactChildren,
+  popover: PopoverProps.Popover,
+  className?: string,
+  style?: CSSProperties,
+  id?: string,
+}
+
+export default class Popover extends PureComponent<PopoverProps, void> {}
