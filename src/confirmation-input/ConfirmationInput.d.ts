7989134753a50eed29d6d499dcc5d0679b024fb8
Andrea Ascari <andrea.ascari@buildo.io>
2017-06-29 15:37:43 +0200

    Definition files for ConfirmationInput component, closes #921.

---
+++ src/confirmation-input/ConfirmationInput.d.ts
@@ -0,0 +1,23 @@
+import { CSSProperties, PureComponent } from 'react'
+
+export type ConfirmationInputProps = {
+  initialValue?: string,
+  onChange?: (value: string) => void,
+  onConfirm?: (value: string) => void,
+  onClear?: () => void,
+  placeholder?: string,
+  disabled?: boolean,
+  text: {
+    clear?: string,
+    toConfirm?: string
+  },
+  icon: {
+    clear?: string,
+    toConfirm?: string
+  },
+  className?: string,
+  id?: string,
+  style?: CSSProperties
+}
+
+export default class ConfirmationInput extends PureComponent<ConfirmationInputProps, void> {}
