fcb6b25d5dd948c6b1aa6967bb604069f69f8e32
Vincenzo Guerrisi <vincenzo@buildo.io>
2017-06-30 18:36:58 +0200

    better typings for BrowserDetector

--- src/browser-detector/BrowserDetector.d.ts
+++ src/browser-detector/BrowserDetector.d.ts
@@ -1,10 +1,11 @@
 import { PureComponent } from 'react';
+import { IBowserGrade } from '@types/bowser';
 
 type Browser = 'chrome' | 'firefox' | 'safari' | 'msie' | 'msedge' | 'opera';
 
 export type BrowserDetectorProps = {
   children: any, // TODO: t.ReactChildren
-  placeholder: (detectedBrowser: string) => any, // TODO: t.ReactChildren
+  placeholder: (detectedBrowser: IBowserGrade) => any, // TODO: t.ReactChildren
   supportedBrowsers?: Browser[],
   userAgent?: string
 }
