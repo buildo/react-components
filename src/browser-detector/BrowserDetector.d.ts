4278b5a4452982cd5e1f71449d6ac6076e053a36
Vincenzo Guerrisi <vincenzo@buildo.io>
2017-07-03 15:06:26 +0200

    do not use @types/bowser

--- src/browser-detector/BrowserDetector.d.ts
+++ src/browser-detector/BrowserDetector.d.ts
@@ -1,13 +1,19 @@
 import { PureComponent } from 'react';
-import { IBowserGrade } from '@types/bowser';
 
 type Browser = 'chrome' | 'firefox' | 'safari' | 'msie' | 'msedge' | 'opera';
 
+export interface DetectedBrowser {    // TODO: use bowser typings
+    name: string;
+    version: string|number;
+    osversion: string|number;
+}
+
+
 export type BrowserDetectorProps = {
   children: any, // TODO: t.ReactChildren
-  placeholder: (detectedBrowser: IBowserGrade) => any, // TODO: t.ReactChildren
+  placeholder: (detectedBrowser: DetectedBrowser) => any, // TODO: t.ReactChildren
   supportedBrowsers?: Browser[],
   userAgent?: string
 }
 
-export default class BrowserDetector extends PureComponent<BrowserDetectorProps, void> {}
+export default class BrowserDetector extends PureComponent<BrowserDetectorProps, {}> {}
