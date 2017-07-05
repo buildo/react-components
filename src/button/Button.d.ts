2c815ad288b26da25643260927ffe1e953877f04
Andrea Ascari <andrea.ascari@buildo.io>
2017-07-05 10:39:54 +0200

    Fixed ts definition for Button.

--- src/button/Button.d.ts
+++ src/button/Button.d.ts
@@ -1,5 +1,6 @@
-import { CSSProperties, PureComponent } from 'react';
+import { ComponentType, CSSProperties, PureComponent } from 'react';
 import { Type } from 'tcomb';
+import { TextOverflowProps } from '../text-overflow';
 
 export namespace ButtonProps {
 
@@ -8,23 +9,25 @@ export namespace ButtonProps {
   type ButtonSize = 'tiny' | 'small' | 'medium';
 
 }
-
+// TODO: props invariants validation
 export type ButtonProps = {
-    onClick: (e: React.SyntheticEvent<HTMLDivElement>) => void,
-    buttonState?: ButtonProps.ButtonState,
-    label?: string | { [key in ButtonProps.ButtonState]?: string },
-    icon?: string | { [key in ButtonProps.ButtonState]?: string },
-    children?: string,
-    type?: ButtonProps.ButtonType,
-    primary?: boolean,
-    flat?: boolean,
-    size?: ButtonProps.ButtonSize,
-    fluid?: boolean,
-    circular?: boolean,
-    textOverflow?: React.ComponentClass<object>, // todo(typo) when translate text overflow, update here its props types
-    style?: CSSProperties,
-    className?: string
-  }
+  onClick: (e: React.SyntheticEvent<HTMLDivElement>) => void,
+  buttonState?: ButtonProps.ButtonState,
+  label?: string | { [key in ButtonProps.ButtonState]?: string },
+  icon?: string | { [key in ButtonProps.ButtonState]?: string },
+  children?: string,
+  type?: ButtonProps.ButtonType,
+  primary?: boolean,
+  flat?: boolean,
+  size?: ButtonProps.ButtonSize,
+  fluid?: boolean,
+  circular?: boolean,
+  textOverflow?: ComponentType<any>, // TODO: should check for textOverflowProsp
+  style?: CSSProperties,
+  className?: string
+}
+
+export type ButtonPropTypes = ButtonProps;
 
 export default class Button extends PureComponent<ButtonProps> {}
 
@@ -32,7 +35,7 @@ export const Props: {
   [key: string]: Type<any>
 }
 
-export const buttonStates: ButtonProps.ButtonState;
-export const buttonSizes: ButtonProps.ButtonSize;
-export const buttonTypes: ButtonProps.ButtonType;
+export const buttonStates: ButtonProps.ButtonState[];
+export const buttonSizes: ButtonProps.ButtonSize[];
+export const buttonTypes: ButtonProps.ButtonType[];
 
