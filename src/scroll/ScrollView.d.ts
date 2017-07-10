28cab0b117fc1ded6037dac331e1e9e009f2972a
Andrea Ascari <andrea.ascari@buildo.io>
2017-07-10 11:27:59 +0200

    Definition files for scroll component.

---
+++ src/scroll/ScrollView.d.ts
@@ -0,0 +1,39 @@
+import { CSSProperties, PureComponent, UIEventHandler } from 'react';
+import { Type } from 'tcomb';
+
+export type ScrollViewProps = {
+  children: any,
+  scrollX?: boolean,
+  scrollY?: boolean,
+  scrollPropagation?: boolean,
+  easing?: 'linear'
+    | 'easeInQuad'
+    | 'easeOutQuad'
+    | 'easeInOutQuad'
+    | 'easeInCubic'
+    | 'easeOutCubic'
+    | 'easeInOutCubic'
+    | 'easeInQuart'
+    | 'easeOutQuart'
+    | 'easeInOutQuart'
+    | 'easeInQuint'
+    | 'easeOutQuint'
+    | 'easeInOutQuint'
+    | 'easeInSine'
+    | 'easeOutSine'
+    | 'easeInOutSine'
+    | 'easeInExpo'
+    | 'easeOutExpo'
+    | 'easeInOutExpo'
+    | 'easeInCirc'
+    | 'easeOutCirc'
+    | 'easeInOutCirc',
+  onScroll?: UIEventHandler<HTMLElement>,
+  [key: string]: any
+}
+
+export const Props: {
+  [key: string]: Type<any>
+}
+
+export default class ScrollView extends PureComponent<ScrollViewProps> {}
