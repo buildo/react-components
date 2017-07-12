906f7243a9c044a258ee4fce13d780613fd19302
Andrea Ascari <andrea.ascari@buildo.io>
2017-07-12 10:55:42 +0200

    Added example for popover's auto positioning.

---
+++ src/popover/examples/hover-auto.js
@@ -0,0 +1,37 @@
+const Option = ({ title, value }) => (
+  <FlexView column hAlignContent='center' className='option'>
+    <FlexView className='title'>{title}</FlexView>
+    <FlexView className='value'>{`$${value}/month`}</FlexView>
+    <Button size='medium' label='Choose' onClick={() => {}} />
+  </FlexView>
+);
+
+const content = (
+  <FlexView>
+    <Option title='Express' value={9} />
+    <Option title='Premium' value={12} />
+    <Option title='Enterprise' value={22} />
+  </FlexView>
+);
+
+class Example extends React.Component {
+
+  render = () => {
+
+    const popoverProps = {
+      content,
+      position: 'auto',
+      anchor: 'start',
+      event: 'hover',
+      className: 'baloon',
+      delay: { whenOpen: 100 }
+    };
+
+    return (
+      <Popover popover={popoverProps}>
+        <FlexView hAlignContent='center' style={{ width: 150, border: '1px solid #dedede', padding: 10 }}>Hover me!</FlexView>
+      </Popover>
+    );
+  }
+
+}
