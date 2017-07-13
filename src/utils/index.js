975f0b9be694c2b501f060ac66a68c546277f747
Andrea Ascari <andrea.ascari@buildo.io>
2017-07-13 14:53:04 +0200

    Optional context for ContextWrapper.

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,14 +9,14 @@ export const stateClassUtil = (...classes) => cx([].concat(...classes).map(cl =>
 export const getContextWrapper = (contextTypes) => {
 
   @props({
-    context: t.Object,
+    context: t.maybe(t.Object),
     children: t.ReactChildren
   })
   class ContextWrapper extends React.Component {
 
     static childContextTypes = contextTypes;
 
-    getChildContext = () => this.props.context;
+    getChildContext = () => this.props.context || {};
 
     render() {
       return this.props.children;
