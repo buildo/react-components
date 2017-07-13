ac64438843a489a2ce431bcfdb15cc472d39f38d
Andrea Ascari <andrea.ascari@buildo.io>
2017-07-13 12:31:44 +0200

    Added utility method getContextWrapper.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,27 @@
+import React from 'react';
 import cx from 'classnames';
+import { props, t } from 'tcomb-react';
 
 export skinnable, { contains } from 'react-skinnable';
-export { props, t } from 'tcomb-react';
+export { props, t };
 export const stateClassUtil = (...classes) => cx([].concat(...classes).map(cl => `is-${cl}`));
+
+export const getContextWrapper = (contextTypes) => {
+
+  @props({
+    context: t.Object,
+    children: t.ReactChildren
+  })
+  class ContextWrapper extends React.Component {
+
+    static childContextTypes = contextTypes;
+
+    getChildContext = () => this.props.context;
+
+    render() {
+      return this.props.children;
+    }
+  }
+
+  return ContextWrapper;
+};
