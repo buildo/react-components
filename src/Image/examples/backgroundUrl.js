bc3f0c1fa89abf71ad245b1c2f953a46fe16b65b
Francesco Cioria <firefelix@gmail.com>
2017-08-18 12:30:12 +0200

    fix image in examples

--- src/Image/examples/backgroundUrl.js
+++ src/Image/examples/backgroundUrl.js
@@ -5,7 +5,7 @@ class Example extends React.Component {
   render() {
     const height = 200;
     const backgroundUrl = getBackgroundUrl(
-      'https://cdn.pixabay.com/photo/2015/03/23/21/11/colored-pencils-686679_1280.jpg',
+      'https://s29.postimg.org/v0nnm9gx3/colored-pencils-686679_1280.jpg',
       { height }
     );
 
--- src/Image/examples/image.js
+++ src/Image/examples/image.js
@@ -5,7 +5,7 @@ class Example extends React.Component {
   render() {
     return (
       <Image
-        src='https://cdn.pixabay.com/photo/2015/03/23/21/11/colored-pencils-686679_1280.jpg'
+        src='https://s29.postimg.org/v0nnm9gx3/colored-pencils-686679_1280.jpg'
         height={200}
         quality={90}
       />
