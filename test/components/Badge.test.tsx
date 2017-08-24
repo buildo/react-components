0569140ad916d89316fdfb18516fd6ebe07a32ac
Gabriele Petronella <gabriele@buildo.io>
2017-08-24 16:05:21 +0200

    Fix Badge tests (woops)

--- test/components/Badge.test.tsx
+++ test/components/Badge.test.tsx
@@ -17,18 +17,18 @@ describe('Badge', () => {
     const badge = shallow(
       <Badge label={42} className='awesome-class' />
     );
-    expect(badge.hasClass('badge'));
-    expect(badge.hasClass('awesome-class'));
-    expect(!badge.hasClass('active'));
+    expect(badge.hasClass('badge')).toBe(true);
+    expect(badge.hasClass('awesome-class')).toBe(true);
+    expect(!badge.hasClass('active')).toBe(true);
   });
 
   it('computes className when active', () => {
     const badge = shallow(
-      <Badge label={42} className='awesome-class' />
+      <Badge label={42} className='awesome-class' active={true} />
     );
-    expect(badge.hasClass('badge'));
-    expect(badge.hasClass('awesome-class'));
-    expect(badge.hasClass('active'));
+    expect(badge.hasClass('badge')).toBe(true);
+    expect(badge.hasClass('awesome-class')).toBe(true);
+    expect(badge.hasClass('active')).toBe(true);
   });
 
 });
