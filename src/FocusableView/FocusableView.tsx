01403428a0b7ba7fdf4033c451f16d855ba75912
Eric Camellini <eric.camellini@buildo.io>
2017-08-25 19:05:27 +0200

    Converted FocusableView - TODO fix createElement problem

--- src/FocusableView/FocusableView.tsx
+++ src/FocusableView/FocusableView.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import * as React from 'react';
 import cx from '../utils/classnames';
-import debounce from 'lodash/debounce';
-import { props, t, skinnable } from '../utils';
+import debounce = require('lodash/debounce');
+import { props, t, ReactChildren } from '../utils';
+
+export type FocusableViewRequiredProps = {
+  /** FocusableView content. If a function it gets called with the boolean "focused" */
+  children: (focused: boolean) => void | JSX.Element,
+  /** When `true` the class "focused" is NOT added */
+  ignoreFocus?: boolean,
+  /** Debounce onFocus/onBlur events of x millis */
+  debounce?: number,
+  className?: string,
+  style?: React.CSSProperties
+}
+
+export type FocusableViewDefaultProps = {
+  /** Callback function called on "focus" event */
+  onFocus: () => void,
+  /** Callback function called on "blur" event */
+  onBlur: () => void,
+  /** "tabindex" attribute */
+  tabIndex: number,
+  /** Wrapper component for `children` */
+  component: keyof React.ReactHTML | React.ComponentClass<{}>
+}
+
+export type FocusableViewProps = Partial<FocusableViewDefaultProps> & FocusableViewRequiredProps
 
 export const Props = {
-  children: t.union([t.ReactChildren, t.Function]),
+  children: t.union([ReactChildren, t.Function]),
   onFocus: t.maybe(t.Function),
   onBlur: t.maybe(t.Function),
   tabIndex: t.maybe(t.Number),
@@ -15,27 +39,25 @@ export const Props = {
   style: t.maybe(t.Object)
 };
 
-/** A panel that can get focus
- * @param children - FocusableView content. If a function it gets called with the boolean "focused"
- * @param onFocus - Callback function called on "focus" event
- * @param onBlur - Callback function called on "blur" event
- * @param tabIndex - "tabindex" attribute
- * @param component - Wrapper component for `children`
- * @param ignoreFocus - When `true` the class "focused" is NOT added
- * @param debounce - Debounce onFocus/onBlur events of x millis
- */
-@skinnable()
-@props(Props, { strict: false })
-export default class FocusableView extends React.Component {
+const defaultProps = {
+  component: 'div',
+  tabIndex: 0,
+  onFocus: () => {},
+  onBlur: () => {}
+};
 
-  static defaultProps = {
-    component: 'div',
-    tabIndex: 0,
-    onFocus: () => {},
-    onBlur: () => {}
-  };
+/**
+ * A panel that can get focus
+ */
+@props(Props, { strict: false })
+export default class FocusableView extends React.Component<FocusableViewProps> {
+
+  getProps() {
+    return { ...defaultProps, ...this.props };
+  }
 
   state = { focused: false };
+  _mounted = false;
 
   componentDidMount() {
     this._mounted = true;
@@ -47,15 +69,15 @@ export default class FocusableView extends React.Component {
 
   _onFocus = () => {
     this.setState({ focused: true });
-    this.props.onFocus();
+    this.getProps().onFocus();
   };
 
   _onBlur = () => {
     this.setState({ focused: false });
-    this.props.onBlur();
+    this.getProps().onBlur();
   };
 
-  _onFocusBlurEvent = (type) => {
+  _onFocusBlurEvent = (type: 'blur' | 'focus') => {
     const { _mounted, state: { focused } } = this;
 
     if (!_mounted) {
@@ -69,10 +91,10 @@ export default class FocusableView extends React.Component {
     }
   }
 
-  onFocusBlurEventDebounced = debounce(this._onFocusBlurEvent, this.props.debounce)
+  onFocusBlurEventDebounced = debounce(this._onFocusBlurEvent, this.getProps().debounce)
 
-  onFocusBlurEvent = ({ type }) => (
-    !t.Nil.is(this.props.debounce) ? this.onFocusBlurEventDebounced(type) : this._onFocusBlurEvent(type)
+  onFocusBlurEvent = ({ type }: { type: 'blur' | 'focus' }) => (
+    !t.Nil.is(this.getProps().debounce) ? this.onFocusBlurEventDebounced(type) : this._onFocusBlurEvent(type)
   )
 
   getLocals() {
@@ -91,7 +113,22 @@ export default class FocusableView extends React.Component {
     };
   }
 
-  template({ children, component, focused, ...locals }) {
+  template() {
+
+    const {
+      onFocusBlurEvent,
+      state: { focused },
+    } = this;
+
+    const { className, ignoreFocus, children, component, ...props } = this.getProps()
+
+    const locals = {
+      ...props,
+      className: !ignoreFocus ? cx(className, { focused }) : className,
+      onFocus: onFocusBlurEvent,
+      onBlur: onFocusBlurEvent
+    }
+
     return React.createElement(
       component,
       locals,
