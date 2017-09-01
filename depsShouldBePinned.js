81233ccc1708d90c31a8fe7ecd88ab727418cc1e
Francesco Cioria <firefelix@gmail.com>
2017-09-01 13:31:15 +0200

    test that deps are always pinned

---
+++ depsShouldBePinned.js
@@ -0,0 +1,16 @@
+const packageJson = require('./package.json');
+const _ = require('lodash');
+
+_.forEach(packageJson.dependencies, (value, key) => {
+  if (!(/^\d+\.\d+\.\d+/).test(value)) {
+    console.error(`\npackage.json.dependencies.${key} is not pinned! it should be in the form of "1.2.3"\n`);
+    process.exit(1);
+  }
+});
+
+_.forEach(packageJson.devDependencies, (value, key) => {
+  if (!(/^\d+\.\d+\.\d+/).test(value)) {
+    console.error(`\npackage.json.devDependencies.${key} is not pinned! it should be in the form of "1.2.3"\n`);
+    process.exit(1);
+  }
+});
