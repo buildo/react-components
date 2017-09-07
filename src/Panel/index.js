de1cc736c53b80a8f032800a98bcd275837bff66
Vincenzo Guerrisi <vincenzo@buildo.io>
2017-09-07 18:24:04 +0200

    convert panel index to ts

--- src/Panel/index.js
+++
@@ -1,6 +0,0 @@
-import Panel from './Panel';
-export { Panel };
-import PanelHeader from './PanelHeader';
-export { PanelHeader };
-import TabbedPanel from './TabbedPanel';
-export { TabbedPanel };
---
+++ src/Panel/index.ts
@@ -0,0 +1,4 @@
+import Panel, { PanelProps } from './Panel';
+import PanelHeader, { PanelHeaderProps } from './PanelHeader';
+import TabbedPanel, { TabbedPanelProps } from './TabbedPanel';
+export { Panel, PanelProps, PanelHeader, PanelHeaderProps, TabbedPanel, TabbedPanelProps };
