a4a7327f6f901e1212c63ef16a9c5b945ebd5281
Vincenzo Guerrisi <vincenzo@buildo.io>
2017-09-22 13:09:59 +0200

    columns reorder plugin

--- src/Tablo/Column/columnUtility.ts
+++ src/Tablo/Column/columnUtility.ts
@@ -8,12 +8,12 @@ import ColumnGroup, { ColumnGroupProps } from '../ColumnGroup';
 // how to extract columns from children, update them and return new grid children
 // useful in the `getLocals` of the several plugins
 
-export interface UpdateProps<T, K extends keyof T> {
+export interface UpdateColumnsProps<T, K extends keyof T> {
   col: React.ReactElement<ColumnProps<T, K>>,
   index: number,
   colGroup?: React.ReactElement<ColumnGroupProps<T>>
 };
-export type UpdateHandler<T, K extends keyof T> = (c: UpdateProps<T, K>) => JSX.Element;
+export type UpdateColumnsHandler<T, K extends keyof T> = (c: UpdateColumnsProps<T, K>) => JSX.Element;
 type TabloChildren<T, K extends keyof T> = React.ReactElement<ColumnProps<T, K>>[] | React.ReactElement<ColumnGroupProps<T>>[];
 
 export const getColumnList = <T, K extends keyof T>(children: TabloChildren<T, K>) => {
@@ -22,7 +22,7 @@ export const getColumnList = <T, K extends keyof T>(children: TabloChildren<T, K
   return thereAreGroups ? flatMap(chArray, ch => Children.toArray(ch.props.children)) : chArray;
 };
 
-export const updateColumns = curry(<T, K extends keyof T>(children: TabloChildren<T, K>, update: UpdateHandler<T, K>): JSX.Element[] => {
+export const updateColumns = curry(<T, K extends keyof T>(children: TabloChildren<T, K>, update: UpdateColumnsHandler<T, K>): JSX.Element[] => {
   const updateIfColumn = (colGroup?: React.ReactElement<ColumnGroupProps<T>>) => (col: React.ReactElement<ColumnProps<T, K>>, index: number) => col.type === Column ? update({ col , index, colGroup }) : col;
   const chArray = Children.toArray(children) as React.ReactElement<ColumnProps<T, K> | ColumnGroupProps<T>>[];
   const thereAreGroups = chArray.filter(ch => ch.type === ColumnGroup).length > 0;
---
+++ src/Tablo/plugins/columnsReorder/DNDHeader.tsx
@@ -0,0 +1,141 @@
+import * as React from 'react';
+import * as cx from 'classnames';
+import { findDOMNode } from 'react-dom';
+import { DropTarget, DragSource, DropTargetSpec, DropTargetCollector, DragSourceSpec, DragSourceCollector } from 'react-dnd';
+import { props, t, ReactChildren } from '../../../utils';
+import FlexView from 'react-flexview';
+import flowRight = require('lodash/flowRight');
+import identity = require('lodash/identity');
+
+export type DNDHeaderProps = {
+  onDragHover?: (source: string, target: string) => void,
+  index: number,
+  name: string,
+  isDragAllowed: boolean,
+  isDropAllowed: (source: string, target: string) => boolean,
+  tabloUniqueId: string,
+  children: React.ReactNode
+};
+
+export type DNDHeaderIntrinsicProps = {
+  connectDragSource: () => void,
+  connectDragPreview: () => void,
+  connectDropTarget: () => void,
+  isDragging: boolean,
+  isOver: boolean,
+  canDrop: boolean,
+  onDrop?: (source: string, target: string) => void,
+}
+
+type DNDMonitorItem = {
+  name: string,
+  index: number
+};
+
+const columnTarget: DropTargetSpec<DNDHeaderProps> = {
+  canDrop({ isDropAllowed, name: target }, monitor) {
+    if (monitor) {
+      const source = (monitor.getItem() as DNDMonitorItem).name;
+      return isDropAllowed(source, target);
+    } else {
+      return false;
+    }
+  },
+  hover({ isDropAllowed, onDragHover, name: target, index: hoverIndex }, monitor, component) {
+    if (monitor && component) {
+      const item = monitor.getItem() as DNDMonitorItem;
+      const source = item.name;
+      if (onDragHover && isDropAllowed(source, target)) {
+        const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+        const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
+        const clientOffset = monitor.getClientOffset();
+        const hoverClientX = clientOffset.x - hoverBoundingRect.left;
+        const dragIndex = item.index;
+
+        if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
+          return;
+        }
+        if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
+          return;
+        }
+        onDragHover(source, target);
+        item.index = hoverIndex;
+      }
+    }
+  },
+  drop({ isDropAllowed, name: target, onDrop }, monitor) {
+    if (monitor) {
+      const source = (monitor.getItem() as DNDMonitorItem).name;
+      if (onDrop && isDropAllowed(source, target)) {
+        onDrop(source, target);
+      }
+    }
+  }
+};
+
+const collectTarget: DropTargetCollector = (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget(),
+  isOver: monitor.isOver(),
+  canDrop: monitor.canDrop()
+});
+
+const columnSource: DragSourceSpec<DNDHeaderProps> = {
+  beginDrag({ name, index }) {
+    return { name, index };
+  },
+  isDragging({ name }, monitor) {
+    if (!monitor) return false;
+    return name === (monitor.getItem() as DNDMonitorItem).name;
+  },
+  canDrag({ isDragAllowed }) {
+    return isDragAllowed;
+  }
+};
+
+const collectSource: DragSourceCollector = (connect, monitor) => {
+  return ({
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  });
+};
+
+const columnType = ({ tabloUniqueId }: DNDHeaderProps) => `${tabloUniqueId}_column`;
+
+@DragSource(columnType, columnSource, collectSource)
+@DropTarget(columnType, columnTarget, collectTarget)
+@props({
+  connectDragSource: t.Function,
+  connectDragPreview: t.Function,
+  isDragAllowed: t.Boolean,
+  isDragging: t.Boolean,
+  //
+  connectDropTarget: t.Function,
+  isOver: t.Boolean,
+  canDrop: t.Boolean,
+  onDrop: t.maybe(t.Function),
+  onDragHover: t.maybe(t.Function),
+  //
+  name: t.String,
+  index: t.Integer,
+  isDropAllowed: t.Function,
+  tabloUniqueId: t.String,
+  children: ReactChildren
+})
+export default class DNDHeader extends React.PureComponent<DNDHeaderProps> {
+
+  render() {
+    const { connectDragSource, connectDropTarget, isDragAllowed, isDragging, canDrop, isOver, children } = this.props;
+    const dndWrapper = isDragAllowed ? flowRight(connectDropTarget, connectDragSource) : identity;
+
+    return dndWrapper(
+      <div className={cx('dndHeader', { isDragging, canDrop, isOver })}>
+        <FlexView vAlignContent='center' width='100%' height='100%'>
+          <FlexView grow height='100%' vAlignContent='center'>
+            {children}
+          </FlexView>
+        </FlexView>
+      </div>
+    );
+  }
+}
