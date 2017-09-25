16e7b120348729706c7fe3e3ff12fdf86335a32a
Vincenzo Guerrisi <vincenzo@buildo.io>
2017-09-25 10:41:27 +0200

    export TabloProps

--- src/Tablo/index.tsx
+++ src/Tablo/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import _Tablo, { TabloProps } from './Tablo';
 import { autosize, columnsResize, columnsReorder, scrollable, selectable, sortable } from './plugins';
 
+export { TabloProps };
 import ColumnGroup, { ColumnGroupProps } from './ColumnGroup';
 export { ColumnGroup, ColumnGroupProps };
 import Column, { ColumnProps } from './Column';
@@ -14,7 +15,6 @@ import Footer, { FooterProps } from './Footer';
 export { Footer, FooterProps };
 
 const Component = autosize(columnsResize(columnsReorder(scrollable(selectable(sortable(_Tablo))))));
-
 export default function Tablo<T, K extends keyof T>(props: TabloProps<T, K>): React.ReactElement<TabloProps<T, K>> {
   return <Component {...props} />;
 }
