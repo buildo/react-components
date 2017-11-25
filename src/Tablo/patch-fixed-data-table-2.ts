f08db03b8ff54aa8156db4a2d605932db1181549
Francesco Cioria <firefelix@gmail.com>
2017-11-25 15:09:14 +0100

    refactor patch-fixed-data-table-2.js in TS

---
+++ src/Tablo/patch-fixed-data-table-2.ts
@@ -0,0 +1,29 @@
+import * as FixedDataTable from 'fixed-data-table-2/internal/FixedDataTable.js';
+import * as FixedDataTableBufferedRows from 'fixed-data-table-2/internal/FixedDataTableBufferedRows.js';
+import * as FixedDataTableCell from 'fixed-data-table-2/internal/FixedDataTableCell.js';
+import * as FixedDataTableCellDefault from 'fixed-data-table-2/internal/FixedDataTableCellDefault.js';
+import * as FixedDataTableCellGroup from 'fixed-data-table-2/internal/FixedDataTableCellGroup.js';
+import * as FixedDataTableColumn from 'fixed-data-table-2/internal/FixedDataTableColumn.js';
+import * as FixedDataTableColumnGroup from 'fixed-data-table-2/internal/FixedDataTableColumnGroup.js';
+import * as FixedDataTableColumnReorderHandle from 'fixed-data-table-2/internal/FixedDataTableColumnReorderHandle.js';
+import * as FixedDataTableColumnResizeHandle from 'fixed-data-table-2/internal/FixedDataTableColumnResizeHandle.js';
+import * as FixedDataTableRow from 'fixed-data-table-2/internal/FixedDataTableRow.js';
+import * as Scrollbar from 'fixed-data-table-2/internal/Scrollbar.js';
+
+import { Table, Column, ColumnGroup, Cell } from 'fixed-data-table-2';
+
+FixedDataTable.propTypes = null;
+FixedDataTableBufferedRows.propTypes = null;
+FixedDataTableCell.propTypes = null;
+FixedDataTableCellDefault.propTypes = null;
+FixedDataTableCellGroup.propTypes = null;
+FixedDataTableColumn.propTypes = null;
+FixedDataTableColumnGroup.propTypes = null;
+FixedDataTableColumnReorderHandle.propTypes = null;
+FixedDataTableColumnResizeHandle.propTypes = null;
+FixedDataTableRow.propTypes = null;
+Scrollbar.propTypes = null;
+(Table as any).propTypes = null;
+(Column as any).propTypes = null;
+(ColumnGroup as any).propTypes = null;
+(Cell as any).propTypes = null;
--- typings/fixed-data-table-2.d.ts
+++ typings/fixed-data-table-2.d.ts
@@ -9,4 +9,15 @@ declare module 'fixed-data-table-2' {
   } from 'fixed-data-table';
 }
 
+declare module 'fixed-data-table-2/internal/FixedDataTable.js';
+declare module 'fixed-data-table-2/internal/FixedDataTableBufferedRows.js';
+declare module 'fixed-data-table-2/internal/FixedDataTableCell.js';
+declare module 'fixed-data-table-2/internal/FixedDataTableCellDefault.js';
+declare module 'fixed-data-table-2/internal/FixedDataTableCellGroup.js';
+declare module 'fixed-data-table-2/internal/FixedDataTableColumn.js';
+declare module 'fixed-data-table-2/internal/FixedDataTableColumnGroup.js';
+declare module 'fixed-data-table-2/internal/FixedDataTableColumnReorderHandle.js';
+declare module 'fixed-data-table-2/internal/FixedDataTableColumnResizeHandle.js';
+declare module 'fixed-data-table-2/internal/FixedDataTableRow.js';
+declare module 'fixed-data-table-2/internal/Scrollbar.js';
 
