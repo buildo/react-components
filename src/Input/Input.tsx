ccbc0f77913fe39ab8038b94183a2a7968b0fb86
Giovanni Gonzaga <giovanni@buildo.io>
2018-03-29 14:59:54 +0200

    add Input.status handling

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -4,14 +4,21 @@ import { props, t, ReactChildren } from '../utils';
 import omit = require('lodash/omit');
 import InputChildren from 'react-input-children';
 import { ObjectOverwrite } from 'typelevel-ts';
+import { Icon } from '../Icon/Icon';
+import View from 'react-flexview';
 
-export type InputRequiredProps = ObjectOverwrite<React.HTMLProps<HTMLInputElement>, {
+export type InputStatus = 'success' | 'failure';
+const InputStatusT = t.enums.of(['success', 'failure']);
+
+export type InputRequiredProps = ObjectOverwrite<InputChildren.Props, {
   /** value */
   value: string,
   /** onChange */
   onChange: (value: string) => void,
   /** input children */
-  children?: React.ReactNode
+  children?: React.ReactNode,
+  /** optional input status */
+  status?: InputStatus
 }>;
 
 export type InputDefaultProps = {
@@ -32,7 +39,8 @@ export const Props = {
   className: t.maybe(t.String),
   id: t.maybe(t.String),
   style: t.maybe(t.Object),
-  children: t.maybe(ReactChildren)
+  children: t.maybe(ReactChildren),
+  status: t.maybe(InputStatusT)
 };
 
 @props(Props, { strict: false })
@@ -48,18 +56,34 @@ export class Input extends React.PureComponent<Input.Props> {
     const {
       className,
       value,
+      status,
+      wrapper = {},
       ...props
     } = this.props as InputDefaultedProps;
     const {
       _onChange: onChange
     } = this;
 
+    const isSuccess = status === InputStatusT('success');
+    const isFailure = status === InputStatusT('failure');
+    const children = this.props.children || (
+      isSuccess ? <View vAlignContent='center'><Icon icon='check-circle' /></View> :
+      isFailure ? <View vAlignContent='center'><Icon icon='times-circle' /></View> :
+      undefined
+    );
+
     const inputProps = {
-      ...omit(props, 'onChange'),
+      ...omit(props, ['onChange', 'status', 'children', 'wrapper']),
+      children,
       value,
       onChange,
       wrapper: {
-        className: cx('input', { 'empty-value': value === '' }, className)
+        ...wrapper,
+        className: cx('input', {
+          'empty-value': value === '',
+          'is-success': isSuccess,
+          'is-failure': isFailure
+        }, wrapper.className, className)
       }
     };
 
