4e8a80afca9ff8cdce2ca30657c6f89434b3a95d
Giovanni Gonzaga <giovanni@buildo.io>
2018-04-19 11:23:13 +0200

    remove react-skinnable dep

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,9 +3,6 @@ import * as cx from 'classnames';
 import { props, ReactElement, ReactChild, ReactChildren, ReactNode } from 'tcomb-react';
 import * as t from 'tcomb';
 
-import skinnable from 'react-skinnable';
-export { skinnable };
-export { contains } from 'react-skinnable';
 export { props, t, ReactElement, ReactChild, ReactChildren, ReactNode };
 export const stateClassUtil = (classes: string[]): string => cx(classes.map(cl => `is-${cl}`));
 
