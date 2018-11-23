a825c7951b07d279b6ef562abe8ee6dccee50448
Francesco Cioria <firefelix@gmail.com>
2018-11-23 12:16:57 +0100

    fix list of prettified files

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,13 +1,19 @@
-const path = require('path');
-const endsWith = require('lodash/endsWith');
-const fs = require('fs');
+const path = require("path");
+const endsWith = require("lodash/endsWith");
+const fs = require("fs");
 
 function brc(name) {
   return path.resolve(__dirname, `src/${name}/${name}`);
 }
 
-const brcComponents = fs.readdirSync(path.resolve(__dirname, 'src'))
-  .filter(c => ['index.ts', '.DS_Store', 'utils', 'Scroll', 'TransitionWrapper'].indexOf(c) === -1)
+const brcComponents = fs
+  .readdirSync(path.resolve(__dirname, "src"))
+  .filter(
+    c =>
+      ["index.ts", ".DS_Store", "utils", "Scroll", "TransitionWrapper"].indexOf(
+        c
+      ) === -1
+  )
   .map(brc);
 
 module.exports = {
@@ -16,24 +22,31 @@ module.exports = {
 
   require: [
     // "global" setup + sass imports
-    path.resolve(__dirname, 'styleguide/setup.ts')
+    path.resolve(__dirname, "styleguide/setup.ts")
   ],
   styleguideComponents: {
-    StyleGuideRenderer: path.join(__dirname, 'styleguide/StyleGuideRenderer.tsx')
+    StyleGuideRenderer: path.join(
+      __dirname,
+      "styleguide/StyleGuideRenderer.tsx"
+    )
   },
 
   // content
-  title: 'buildo-react-components',
-  template: 'styleguide/index.html',
-  propsParser: require('react-docgen-typescript').parse, // detect docs using TS information
-  sections: [{
-    name: 'Components',
-    components: () => brcComponents
-  }],
+  title: "buildo-react-components",
+  template: "styleguide/index.html",
+  propsParser: require("react-docgen-typescript").parse, // detect docs using TS information
+  sections: [
+    {
+      name: "Components",
+      components: () => brcComponents
+    }
+  ],
   showCode: true,
   showUsage: false, // show props by default
   getExampleFilename(componentPath) {
-    const name = endsWith(componentPath, '.tsx') ? path.basename(componentPath, '.tsx') : path.basename(componentPath, '.ts');
+    const name = endsWith(componentPath, ".tsx")
+      ? path.basename(componentPath, ".tsx")
+      : path.basename(componentPath, ".ts");
     return path.resolve(__dirname, `src/${name}/Examples.md`);
   }
 };
