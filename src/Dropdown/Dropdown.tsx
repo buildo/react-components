56f852f5b732702d031f1d353f953233216d1375
Vincenzo Guerrisi <vincenzo@buildo.io>
2019-02-16 01:24:33 +0100

    remove custom components, fix ref and style

--- src/Dropdown/Dropdown.tsx
+++ src/Dropdown/Dropdown.tsx
@@ -1,56 +1,18 @@
 import * as React from "react";
-import Select, { components } from "react-select";
+import Select from "react-select";
 import * as SelectNS from "react-select/lib/Select";
 import * as cx from "classnames";
 
 export namespace Dropdown {
-  export type Props<OptionType> = SelectNS.Props<OptionType> & { size: "medium" | "small", flat: boolean };
+  export type Props<OptionType> = SelectNS.Props<OptionType> & {
+    size: "medium" | "small";
+    flat: boolean;
+    innerRef?: (ref: Select<OptionType> | null) => void;
+  };
 }
 
 const defaultComponents: Dropdown.Props<any>["components"] = {
-  Placeholder: ({ children }) => (
-    <div className="dropdown__placeholder">{children}</div>
-  ),
-  IndicatorSeparator: () => null,
-  Option: props => (
-    <components.Option
-      {...props}
-      className={cx({
-        "is-disabled": props.isDisabled,
-        "is-focused": props.isFocused,
-        "is-selected": props.isSelected
-      })}
-    />
-  ),
-  DropdownIndicator: props => (
-    <span className="dropdown__select-arrow-zone">
-      <span
-        className={
-          props.selectProps.menuIsOpen
-            ? "dropdown__select-arrow-up"
-            : "dropdown__select-arrow-down"
-        }
-      />
-    </span>
-  ),
-  MultiValueLabel: ({ children }) => (
-    <div className="dropdown__multi-value-label">{children}</div>
-  ),
-  MultiValueRemove: props => (
-    <components.MultiValueRemove
-      innerProps={{
-        ...props.innerProps,
-        className: "dropdown__multi-value-remove"
-      }}
-    >
-      ×
-    </components.MultiValueRemove>
-  ),
-  ClearIndicator: props => (
-    <span onClick={props.clearValue} className="dropdown__clear-indicator">
-      ×
-    </span>
-  )
+  IndicatorSeparator: () => null
 };
 
 export class Dropdown<OptionType> extends React.Component<
@@ -70,32 +32,18 @@ export class Dropdown<OptionType> extends React.Component<
   };
 
   getCustomClassNames() {
-    const {
-      size,
-      flat,
-      isClearable,
-      isMulti,
-      isDisabled
-    } = this.props;
+    const { size, flat, isMulti } = this.props;
     return cx({
       "is-medium": size === "medium",
       "is-small": size === "small",
       "is-flat": flat,
-      "is-multi": isMulti,
-      "is-clearable": isClearable,
-      "is-disabled": isDisabled
+      "is-multi": isMulti
     });
   }
 
-  private select: Select<OptionType> | null;
-
-  focus = () => {
-    this.select && this.select.focus();
-  };
-
   render() {
     const {
-      props: { className, components: customComponents, ...props }
+      props: { className, components: customComponents, innerRef, ...props }
     } = this;
 
     return (
@@ -107,9 +55,7 @@ export class Dropdown<OptionType> extends React.Component<
           ...customComponents
         }}
         className={cx("dropdown", className, this.getCustomClassNames())}
-        ref={select => {
-          this.select = select;
-        }}
+        ref={innerRef}
       />
     );
   }
