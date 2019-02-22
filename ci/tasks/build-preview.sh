4c65e215734bf55a4c23b126a4b8feebab6b86b4
Eric Camellini <eric.camellini@buildo.io>
2019-02-22 18:57:32 +0100

    Adding missing branch extraction

--- ci/tasks/build-preview.sh
+++ ci/tasks/build-preview.sh
@@ -3,6 +3,12 @@
 set -e
 set -x
 
+
+apt-get update
+apt-get install -y jq
+
+BRANCH=$(cat react-components/.git/resource/metadata.json | jq -r '.[] | select(.name == "head_name").value')
+
 cd react-components
 
 yarn install --no-progress
