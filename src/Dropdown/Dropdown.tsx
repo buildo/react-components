2122a7a8ec52a0fd90e00e5bc345e6bf074a22f5
Vincenzo Guerrisi <vincenzo@buildo.io>
2019-02-22 10:29:03 +0100

    better types for value/onChange

--- src/Dropdown/Dropdown.tsx
+++ src/Dropdown/Dropdown.tsx
@@ -2,49 +2,94 @@ import * as React from "react";
 import Select from "react-select";
 import * as SelectNS from "react-select/lib/Select";
 import * as cx from "classnames";
+import { ObjectOmit } from "src/utils";
 
 export namespace Dropdown {
-  export type Props<OptionType> = SelectNS.Props<OptionType> & {
+  export type Props<OptionType extends {}> = ObjectOmit<
+    SelectNS.Props<OptionType>,
+    "isMulti" | "isClearable" | "onChange" | "value"
+  > & {
     size: "medium" | "small";
     flat: boolean;
     innerRef?: (ref: Select<OptionType> | null) => void;
-  };
+  } & (
+      | {
+          type: "multi" | "multi-clearable";
+          value: OptionType[];
+          onChange: (value: OptionType[]) => void;
+        }
+      | {
+          type: "single";
+          value: OptionType | null;
+          onChange: (value: OptionType) => void;
+        }
+      | {
+          type: "single-clearable";
+          value: OptionType | null;
+          onChange: (value: OptionType | null) => void;
+        });
 }
-
-const defaultComponents: Dropdown.Props<any>["components"] = {
-  IndicatorSeparator: () => null
-};
 
 export class Dropdown<OptionType> extends React.Component<
   Dropdown.Props<OptionType>
 > {
-  static defaultProps: Dropdown.Props<any> = {
+  static defaultProps: Partial<Dropdown.Props<{}>> = {
     delimiter: ",",
     size: "medium",
     isDisabled: false,
     isSearchable: false,
-    isClearable: false,
-    isMulti: false,
     flat: false,
     blurInputOnSelect: true,
     menuPlacement: "bottom",
     components: {}
   };
 
+  defaultComponents: Dropdown.Props<OptionType>["components"] = {
+    IndicatorSeparator: () => null
+  };
+
   getCustomClassNames() {
-    const { size, flat, isMulti, isSearchable } = this.props;
+    const { size, flat, type, isSearchable } = this.props;
     return cx({
       "is-medium": size === "medium",
       "is-small": size === "small",
       "is-flat": flat,
-      "is-multi": isMulti,
+      "is-multi": type === "multi",
       "is-searchable": isSearchable
     });
   }
 
+  isMulti = (): boolean => {
+    switch (this.props.type) {
+      case "multi":
+      case "multi-clearable":
+        return true;
+      case "single":
+      case "single-clearable":
+        return false;
+    }
+  };
+
+  isClearable = (): boolean => {
+    switch (this.props.type) {
+      case "single-clearable":
+      case "multi-clearable":
+        return true;
+      case "single":
+      case "multi":
+        return false;
+    }
+  };
+
   render() {
     const {
-      props: { className, components: customComponents, innerRef, ...props }
+      props: {
+        className,
+        components: customComponents,
+        innerRef,
+        type,
+        ...props
+      }
     } = this;
 
     return (
@@ -52,11 +97,13 @@ export class Dropdown<OptionType> extends React.Component<
         {...props}
         classNamePrefix="dropdown"
         components={{
-          ...defaultComponents,
+          ...this.defaultComponents,
           ...customComponents
         }}
         className={cx("dropdown", className, this.getCustomClassNames())}
         ref={innerRef}
+        isMulti={this.isMulti()}
+        isClearable={this.isClearable()}
       />
     );
   }
