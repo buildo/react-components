6fca16f10e8348b72ce63ebbe6d78ac0206fd8d5
Eric Camellini <eric.camellini@buildo.io>
2019-02-22 21:52:42 +0100

    Fixing missing folder error

--- ci/tasks/build-preview.sh
+++ ci/tasks/build-preview.sh
@@ -15,5 +15,6 @@ yarn install --no-progress
 
 NODE_ENV=production yarn build-styleguidist
 
+mkdir ../preview/build
 cp -a styleguide/build/* ../preview/build/*
 cp -a styleguide/index.html ../preview/$BRANCH.html
