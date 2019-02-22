f858e7dcb0c7e8d0b53fcbd9d524e567fc8acf55
Eric Camellini <eric.camellini@buildo.io>
2019-02-22 14:27:02 +0100

    Adding preview to CI

---
+++ ci/tasks/build-preview.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+set -x
+
+cd react-components
+
+yarn install --no-progress
+
+ yarn build-styleguidist
+
+mv styleguide/index.html styleguide/$BRANCH.html
+
+cp -a styleguide/* ../preview
