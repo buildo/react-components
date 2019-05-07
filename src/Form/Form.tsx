dc6680f267213f97d5fad223acaf9ed40e7aa567
Giovanni Gonzaga <giovanni@buildo.io>
2019-05-07 15:14:16 +0200

    simplify Form API and use some ugly hacks pass a button ref around

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,72 +1,40 @@
 import * as React from "react";
 import * as cx from "classnames";
-import { StatefulButton } from "../Button/StatefulButton";
 import View from "react-flexview";
+import Button, { StatefulButton } from "../Button";
+import { findDOMNode } from "../utils";
 
 export namespace Form {
   export type Props = View.Props & {
-    onSubmit: () => Promise<unknown>;
-    submitLabel: string;
-    submitProcessingLabel?: string;
-    renderSubmit?: (buttonProps: StatefulButton.Props) => JSX.Element;
+    render: (ref: React.RefObject<any>) => NonNullable<React.ReactNode>;
   };
 }
 
 export class Form extends React.PureComponent<Form.Props> {
-  formRef = React.createRef<HTMLFormElement>();
+  buttonRef = React.createRef<Button | StatefulButton>();
 
   onFormSubmit: React.ReactEventHandler<HTMLFormElement> = e => {
-    if (this.formRef.current) {
-      const button = this.formRef.current.querySelector<HTMLDivElement>(
-        ".button .button-inner"
-      );
-      if (button) {
-        button.click();
+    if (this.buttonRef.current) {
+      const buttonDiv = findDOMNode(this.buttonRef.current);
+      const clickable = buttonDiv.querySelector(".button-inner");
+      if (clickable) {
+        (clickable as HTMLDivElement).click();
       }
     }
     e.stopPropagation();
     e.preventDefault();
   };
 
-  renderSubmit = (props: StatefulButton.Props): JSX.Element =>
-    this.props.renderSubmit ? (
-      this.props.renderSubmit(props)
-    ) : (
-      <StatefulButton {...props} />
-    );
-
   render() {
-    const {
-      children,
-      className,
-      submitLabel,
-      submitProcessingLabel,
-      onSubmit,
-      renderSubmit,
-      ...props
-    } = this.props;
-    const submitButtonProps: StatefulButton.Props = {
-      style: { marginTop: "12px" },
-      primary: true,
-      baseState: "ready",
-      onClick: this.props.onSubmit,
-      label: {
-        ready: submitLabel,
-        processing: submitProcessingLabel || submitLabel
-      }
-    };
+    const { className, style, render, ...props } = this.props;
 
     return (
       <form
         className={cx("form", className)}
         onSubmit={this.onFormSubmit}
-        style={{ width: "100%" }}
-        ref={this.formRef}
+        style={{ width: "100%", ...style }}
       >
-        <View {...props}>
-          {children}
-          {this.renderSubmit(submitButtonProps)}
-        </View>
+        <View {...props}>{render(this.buttonRef)}</View>
         <input type="submit" style={{ display: "none" }} />
       </form>
     );
