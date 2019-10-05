1776e1739162dd2be214eb8b2b91254f80785ded
jenipha <jenipharachel@gmail.com>
2019-10-05 15:40:59 +0530

    moved badge component's test within src

--- src/Badge/__tests__/Badge.test.tsx
+++ src/Badge/__tests__/Badge.test.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as renderer from "react-test-renderer";
 import { shallow } from "enzyme";
 
-import Badge from "../../src/Badge";
+import Badge from "..";
 
 let consoleError: jest.SpyInstance<{}>;
 
