18eab9d11dd868601bf5c9b6dbe5387a33d5630e
Giovanni Gonzaga <giovanni@buildo.io>
2020-01-02 17:48:09 +0100

    remove tcomb, tcomb-props deps and related utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as cx from "classnames";
-import {
-  props,
-  ReactElement,
-  ReactChild,
-  ReactChildren,
-  ReactNode
-} from "tcomb-react";
-import * as t from "tcomb";
 import _find = require("lodash/find");
 
-export { props, t, ReactElement, ReactChild, ReactChildren, ReactNode };
 export const stateClassUtil = (classes: string[]): string =>
   cx(classes.map(cl => `is-${cl}`));
 
@@ -23,10 +14,6 @@ type Props = {
 export const getContextWrapper = (
   contextTypes = {}
 ): React.ComponentType<Props> => {
-  @props({
-    context: t.maybe(t.Object),
-    children: ReactChildren
-  })
   class ContextWrapper extends React.Component<Props> {
     static childContextTypes = contextTypes;
 
